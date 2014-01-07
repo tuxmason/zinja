060e167aba7f8618d7ed752809b9c582fca09263
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 21:54:10 +0300

    packages/stage-two: gzip 1.6

---
+++ packages/stage-two/gzip/config.mk
@@ -0,0 +1,12 @@
+PKGNAME := gzip
+PKGVER := 1.6
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--bindir=/bin \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+CC := "${CC} "

aceac20b264a2df8ae1ea939f38d12f6f97e62ab
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 20:10:32 +0300

    packages/stage-two: add mpfr 3.1.2

---
+++ packages/stage-two/mpfr/config.mk
@@ -0,0 +1,12 @@
+PKGNAME := mpfr
+PKGVER := 3.1.2
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--enable-shared \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+CC := "${CC} "

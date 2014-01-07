4469b52c59720f2d418cf688f10d3063d4f3d484
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 20:42:39 +0300

    packages/stage-two: add sed 4.2.2

---
+++ packages/stage-two/sed/config.mk
@@ -0,0 +1,13 @@
+PKGNAME := sed
+PKGVER := 4.2.2
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--disable-i18n \
+	--disable-nls \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+CC := "${CC} "

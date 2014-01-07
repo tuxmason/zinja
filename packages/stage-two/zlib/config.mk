93255ad23efab77dac273b3d82885164cfcb2358
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 20:22:22 +0300

    packages/stage-two: add zlib 1.2.8

---
+++ packages/stage-two/zlib/config.mk
@@ -0,0 +1,11 @@
+PKGNAME := zlib
+PKGVER := 1.2.8
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	 --sharedlibdir=/lib \
+	 --shared
+CC := "${CC} "

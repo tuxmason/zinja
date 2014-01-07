78e0e70ec5202b61c9890df550b9687fb2406883
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 21:41:03 +0300

    packages/stage-two: add diffutils 3.3

---
+++ packages/stage-two/diffutils/config.mk
@@ -0,0 +1,12 @@
+PKGNAME := diffutils
+PKGVER := 3.3
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+CC := "${CC} "
+CXX := "${CXX} "

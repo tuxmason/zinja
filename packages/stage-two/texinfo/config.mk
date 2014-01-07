a023ae10ad719af294bd2815ce95df5cc3923d1b
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 22:19:30 +0300

    packages/stage-two: texinfo 5.2

---
+++ packages/stage-two/texinfo/config.mk
@@ -0,0 +1,12 @@
+PKGNAME := texinfo
+PKGVER := 5.2
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr  \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+CC := "${CC} "
+CXX := "${CXX} "

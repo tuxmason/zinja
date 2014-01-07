41f8022def0f8191896bcb2a17de59536a424c17
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 22:10:30 +0300

    packages/stage-two: libee 0.4.1

---
+++ packages/stage-two/libee/config.mk
@@ -0,0 +1,14 @@
+PKGNAME := libee
+PKGVER := 0.4.1
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache
+CC := "${CC} "
+CXX := "${CXX} "
+PKGCONFIG := "$(SYSROOTDIR)/usr/lib/pkgconfig"

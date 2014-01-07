4e8f824889de3035f10ffefa2bb9ecdfece4a189
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 22:11:09 +0300

    packages/stage-two: json-c 0.11

---
+++ packages/stage-two/json-c/config.mk
@@ -0,0 +1,14 @@
+PKGNAME := json-c
+PKGVER := 0.11
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--enable-shared \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache
+CC := "${CC} "
+CXX := "${CXX} "

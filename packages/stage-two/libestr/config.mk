d0878eabea4ce42091bef3363575cb645a005b08
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 22:09:42 +0300

    packages/stage-two: libestr 0.1.9

---
+++ packages/stage-two/libestr/config.mk
@@ -0,0 +1,13 @@
+PKGNAME := libestr
+PKGVER := 0.1.9
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache
+CC := "${CC} "
+CXX := "${CXX} "

5389c460961453376e123b29f9b1848dc16ef5ac
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 21:47:31 +0300

    packages/stage-two: add findutils 4.5.12

---
+++ packages/stage-two/findutils/config.mk
@@ -0,0 +1,12 @@
+PKGNAME := findutils
+PKGVER := 4.5.12
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache
+CC := "${CC} "

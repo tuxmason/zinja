22f889020f794ffd9f40a152d07e5a388eaf1b37
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 21:43:44 +0300

    packages/stage-two: add gawk 4.1.0

---
+++ packages/stage-two/gawk/config.mk
@@ -0,0 +1,12 @@
+PKGNAME := gawk
+PKGVER := 4.1.0
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--libexecdir=/usr/lib \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+CC := "${CC} "

df87f03755c720212e501e1da91e817a1b6d52b8
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 21:18:14 +0300

    packages/stage-two: add bison 3.0.2

---
+++ packages/stage-two/bison/config.mk
@@ -0,0 +1,13 @@
+PKGNAME := bison
+PKGVER := 3.0.2
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache
+CC := "${CC} "
+LIBS := "-lrt "

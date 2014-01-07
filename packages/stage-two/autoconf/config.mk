e4e49e6e2a5db01f29a4bd878a4a07427c752a1f
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 21:29:50 +0300

    packages/stage-two: add autoconf 2.69

---
+++ packages/stage-two/autoconf/config.mk
@@ -0,0 +1,12 @@
+PKGNAME := autoconf
+PKGVER := 2.69
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+CC := "${CC} "
+CXX := "${CXX} "

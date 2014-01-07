730e523bb596664fd353af7354bb7f396e17df69
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 21:27:14 +0300

    packages/stage-two: add libtool 2.4.2

---
+++ packages/stage-two/libtool/config.mk
@@ -0,0 +1,12 @@
+PKGNAME := libtool
+PKGVER := 2.4.2
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--disable-static \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+CC := "${CC} "

7d9d7148b8f9b0ab7e13ff180981aad6351c553d
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 20:21:18 +0300

    packages/stage-two: add mpc 1.0.1

---
+++ packages/stage-two/mpc/config.mk
@@ -0,0 +1,12 @@
+PKGNAME := mpc
+PKGVER := 1.0.1
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--enable-shared \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+CC := "${CC} "

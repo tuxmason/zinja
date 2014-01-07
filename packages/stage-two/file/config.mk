0ac321d7184a7055d101bc145c79f43610fe2cef
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 21:41:48 +0300

    packages/stage-two: add file 5.15

---
+++ packages/stage-two/file/config.mk
@@ -0,0 +1,12 @@
+PKGNAME := file
+PKGVER := 5.15
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+CC := "${CC} "
+CXX := "${CXX} "

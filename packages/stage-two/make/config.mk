bba8a9f58909dad7a59964d8d993379a7aa9a683
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 22:01:46 +0300

    packages/stage-two: make 4.0

---
+++ packages/stage-two/make/config.mk
@@ -0,0 +1,12 @@
+PKGNAME := make
+PKGVER := 4.0
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--without-guile \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+CC := "${CC} "

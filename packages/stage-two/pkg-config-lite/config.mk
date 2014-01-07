956a4c4461a6e8c7b66f3a09cb691a4b54763e0a
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-07 20:48:45 +0300

    packages/stage-two: add pkg-config-lite 0.28-1

---
+++ packages/stage-two/pkg-config-lite/config.mk
@@ -0,0 +1,11 @@
+PKGNAME := pkg-config-lite
+PKGVER := 0.28-1
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+CC := "${CC} "

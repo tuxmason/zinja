153b15437e27aa1b92a2a69caccc4eb2732bf6de
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2014-01-13 14:29:23 +0300

    stage-two/ncurses: build both wide and non wide char libs

--- packages/stage-two/ncurses/config.mk
+++ packages/stage-two/ncurses/config.mk
@@ -2,18 +2,25 @@ PKGNAME := ncurses
 PKGVER := 5.9
 PKGSRC := ncurses-5.9.tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
-PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-final-obj
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-tgt-obj
+WPKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-tgt-wc-obj
+NWPKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-tgt-nwc-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+WCOPTS := --enable-widec
+NWCOPTS := --without-progs
 COPTS := --prefix=/usr \
 	--with-shared \
 	--without-debug \
 	--without-ada \
+	--libdir=/lib \
 	--enable-pc-files \
 	--enable-overwrite \
 	--with-build-cc=gcc \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \
+	--without-cxx-binding \
+	--with-pkg-config-libdir=/usr/lib/pkgconfig \
 	--with-default-terminfo-dir=/usr/share/terminfo \
 	--with-terminfo-dirs="/etc/terminfo:/usr/share/terminfo"
 CC := "${CC} "

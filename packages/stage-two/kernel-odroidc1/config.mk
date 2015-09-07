20489d3a6dfff0db2d80da170a889e3a2753171e
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2015-09-07 22:53:45 +0300

    packages/stage-two: odroid-c1 kernel

---
+++ packages/stage-two/kernel-odroidc1/config.mk
@@ -0,0 +1,8 @@
+PKGNAME := linux
+PKGVER := odroidc-3.10.y
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PATCHLIST := $(PATCHDB)/kernel/$(PKGNAME)-list.txt
+PATCHDIR := $(PATCHDB)/kernel
+CONF := $(PATCHDB)/kernel/kconfig-odroidc1.arm
+PKGOBJDIR := $(TCBUILDROOT)/kernel-obj

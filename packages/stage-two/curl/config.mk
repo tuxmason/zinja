85a7e8f678ef64c2db5ee4c6860b01a4479e648f
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2015-10-24 22:12:31 +0300

    packages/stage-two: add curl 7.45.0

---
+++ packages/stage-two/curl/config.mk
@@ -0,0 +1,14 @@
+PKGNAME := curl
+PKGVER := 7.45.0
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--enable-threaded-resolver
+
+CC := "${CC} "
+CXX := "${CXX} "

46b14832f48ece856f4cbd6e6d78f7bdefce8f50
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2015-10-24 19:19:04 +0300

    packages/stage-two: add libmicrohttpd 0.9.38

---
+++ packages/stage-two/libmicrohttpd/config.mk
@@ -0,0 +1,12 @@
+PKGNAME := libmicrohttpd
+PKGVER := 0.9.38
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "

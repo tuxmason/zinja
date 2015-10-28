a89ba4c1d55dca3fa65fa8c6fe884b1ea5b89746
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2015-10-28 22:59:57 +0300

    packages/stage-two: add libedit 20150325-3.1

---
+++ packages/stage-two/libedit/config.mk
@@ -0,0 +1,13 @@
+PKGNAME := libedit
+PKGVER := 20150325-3.1
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--enable-widec \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "

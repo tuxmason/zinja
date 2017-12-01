0b316903aa3271597ca37dbad51e7608f0611407
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-01 23:25:53 +0300

    stage-one/pkg-config-lite: add musl support
    - add SRCURL
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-one/pkg-config-lite/config.mk
+++ packages/stage-one/pkg-config-lite/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := pkg-config-lite
 PKGVER := 0.28-1
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://sourceforge.net/projects/pkgconfiglite/files/$(PKGVER)/$(PKGSRC)
 COPTS := --prefix=$(CROSSTOOLS) \
 	--host=$(TARGETARCH) \
 	--with-pc-path="$(SYSROOTDIR)/usr/lib/pkgconfig:$(SYSROOTDIR)/usr/share/pkgconfig"

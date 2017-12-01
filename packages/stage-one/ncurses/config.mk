fcafa2dab76cb39dc35e489bd2861d97127af224
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-01 23:11:55 +0300

    stage-one/ncurses: upgrade to version 6.0
    - added SRCURL
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-one/ncurses/config.mk
+++ packages/stage-one/ncurses/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := ncurses
-PKGVER := 5.9
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGVER := 6.0
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-stage-one-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://ftp.gnu.org/gnu/ncurses/$(PKGSRC)
 COPTS := --prefix=$(CROSSTOOLS) \
 	--without-shared \
 	--without-debug

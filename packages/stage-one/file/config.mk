0f7a7434dee2aee43b8a20eb8d38e7105cb1fe45
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-01 22:50:06 +0300

    stage-one/file: upgrade to version 5.32
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-one/file/config.mk
+++ packages/stage-one/file/config.mk
@@ -1,9 +1,10 @@
 PKGNAME := file
-PKGVER := 5.15
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGVER := 5.32
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-stage-one-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := ftp://ftp.astron.com/pub/file/$(PKGSRC)
 COPTS := --prefix=$(CROSSTOOLS) \
 	--disable-static

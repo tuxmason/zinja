d55ec1fe424a355dc4519feff7fb03b68bbe474c
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-02 21:02:16 +0300

    stage-one/mpfr: upgrade to version 3.1.6
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-one/mpfr/config.mk
+++ packages/stage-one/mpfr/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := mpfr
-PKGVER := 3.1.2
+PKGVER := 3.1.6
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-stage-one-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://www.mpfr.org/mpfr-$(PKGVER)/$(PKGSRC)
 COPTS := --prefix=$(CROSSTOOLS) \
 	--disable-static \
 	--with-gmp=$(CROSSTOOLS)

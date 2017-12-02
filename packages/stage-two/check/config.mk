18c8693829745f0471bb51af4459cc6fefa63cd7
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-03 00:44:03 +0300

    stage-two/check: upgrade to version 0.11.0
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/check/config.mk
+++ packages/stage-two/check/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := check
-PKGVER := 0.9.11
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGVER := 0.11.0
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://github.com/libcheck/check/releases/download/$(PKGVER)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)

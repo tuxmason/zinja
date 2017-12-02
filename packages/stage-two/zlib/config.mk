f6cc7d648517d69da8489ed16b7081e1d4fe10e9
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-02 19:36:19 +0300

    stage-two/zlib: upgrade to version 1.2.11
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/zlib/config.mk
+++ packages/stage-two/zlib/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := zlib
-PKGVER := 1.2.8
+PKGVER := 1.2.11
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://zlib.net/$(PKGSRC)
 COPTS := --prefix=/usr \
 	 --sharedlibdir=/lib \
 	 --shared

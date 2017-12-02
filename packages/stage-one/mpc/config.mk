88a3a3236d21fa490b9a8116865321e1a4b08230
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-02 20:59:53 +0300

    stage-one/mpc: upgrade to version 1.0.3
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-one/mpc/config.mk
+++ packages/stage-one/mpc/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := mpc
-PKGVER := 1.0.1
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGVER := 1.0.3
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-stage-one-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://www.multiprecision.org/mpc/download/$(PKGVER)
 COPTS := --prefix=$(CROSSTOOLS) \
 	--disable-static \
 	--with-gmp=$(CROSSTOOLS) \

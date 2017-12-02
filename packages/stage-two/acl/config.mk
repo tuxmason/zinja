2036facf06aadefa9ee51fb808edf10d2ba988e9
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-03 01:16:37 +0300

    stage-two/acl: add musl lib C support
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/acl/config.mk
+++ packages/stage-two/acl/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := acl
 PKGVER := 2.2.52
-PKGSRC := $(PKGNAME)-$(PKGVER).src.tar.xz
+PKGSRC := $(PKGNAME)-$(PKGVER).src.tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://download.savannah.gnu.org/releases/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--libdir=/lib \
 	--disable-gettext \

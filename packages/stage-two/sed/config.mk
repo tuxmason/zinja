9a9aa83a439077b491b7130cf331ce5150aff7cc
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-02 23:19:17 +0300

    stage-two/sed: upgrade to version 4.4
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/sed/config.mk
+++ packages/stage-two/sed/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := sed
-PKGVER := 4.2.2
+PKGVER := 4.4
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--disable-i18n \
 	--disable-nls \

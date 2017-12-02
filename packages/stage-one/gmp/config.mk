5759c1f54ecd19a49e980a7178171382f9bf9b88
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-02 20:01:41 +0300

    stage-two/gmp: upgrade to version 6.1.2
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-one/gmp/config.mk
+++ packages/stage-one/gmp/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := gmp
-PKGVER := 5.1.3
+PKGVER := 6.1.2
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/gmp/$(PKGSRC)
 COPTS := --prefix=$(CROSSTOOLS) \
 	--enable-cxx \
 	--disable-static

ef97bf44c4c135dc842d05480302df8d6ecc62f8
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-02 19:57:47 +0300

    stage-one/binutils: upgrade to version 2.29.1
    - add SRCURL var
    - enable 64-bit support
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-one/binutils/config.mk
+++ packages/stage-one/binutils/config.mk
@@ -1,14 +1,19 @@
 PKGNAME := binutils
-PKGVER := 2.23.2
+PKGVER := 2.29.1
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=$(CROSSTOOLS) \
 	--disable-nls \
 	--disable-static \
 	--disable-multilib \
+	--enable-64-bit-bfd \
+	--enable-gold=yes \
+	--enable-threads \
+	--disable-werror \
 	--host=$(HOSTARCH) \
 	--target=$(TARGETARCH) \
 	--with-sysroot=$(SYSROOTDIR) \

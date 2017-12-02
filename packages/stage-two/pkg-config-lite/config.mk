287f81bffd9b982574de6c9f773ac3905ab30985
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-03 00:36:06 +0300

    stage-two/pkg-config-lite: add source URL
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/pkg-config-lite/config.mk
+++ packages/stage-two/pkg-config-lite/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := pkg-config-lite
 PKGVER := 0.28-1
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://sourceforge.net/projects/pkgconfiglite/files/$(PKGVER)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)

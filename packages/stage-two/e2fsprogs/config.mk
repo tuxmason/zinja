4842f4b221363a246c42021d61a89f743850c0a9
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-03 14:27:36 +0300

    stage-two/e2fsprogs: upgrade to version 1.43.7
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/e2fsprogs/config.mk
+++ packages/stage-two/e2fsprogs/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := e2fsprogs
-PKGVER := 1.42.9
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGVER := 1.43.7
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v$(PKGVER)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--with-root-prefix="" \
 	--enable-elf-shlibs \

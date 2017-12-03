645774ab9dfdaf955c2119831234b348534fe586
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 00:01:01 +0300

    stage-two/libtool: upgrade to version 2.4.6
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libtool/config.mk
+++ packages/stage-two/libtool/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := libtool
-PKGVER := 2.4.2
+PKGVER := 2.4.6
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--disable-static \
 	--build=$(BUILDARCH) \

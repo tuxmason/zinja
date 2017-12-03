c6f809b2ba73d0c908b58c62ba4a165e967c99ba
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-03 20:05:22 +0300

    stage-two/bison: upgrade to version 3.0.4
    - don't set LIBS as it is not needed when building against musl
      lib C
    
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/bison/config.mk
+++ packages/stage-two/bison/config.mk
@@ -1,13 +1,14 @@
 PKGNAME := bison
-PKGVER := 3.0.2
+PKGVER := 3.0.4
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \
-	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache
+	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache \
+	--docdir=/usr/share/doc/$(PKGNAME)-$(PKGVER)
 CC := "${CC} "
-LIBS := "-lrt "

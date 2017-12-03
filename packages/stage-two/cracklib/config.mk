aa7f7bd5ad368653154faddd3f1afca3d1405c90
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-03 14:35:51 +0300

    stage-two/cracklib: upgrade to version 2.9.6
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/cracklib/config.mk
+++ packages/stage-two/cracklib/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := cracklib
-PKGVER := 2.9.1
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGVER := 2.9.6
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://github.com/cracklib/cracklib/releases/download/$(PKGNAME)-$(PKGVER)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--disable-static \
 	--without-python \

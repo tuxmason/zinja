a0afbc3624f6fb82cedad924403cbadab89bb66d
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-03 19:51:33 +0300

    stage-two/m4: upgrade to version 1.4.18
    - don't set LIBS as it is not needed
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/m4/config.mk
+++ packages/stage-two/m4/config.mk
@@ -1,13 +1,13 @@
 PKGNAME := m4
-PKGVER := 1.4.17
+PKGVER := 1.4.18
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--host=$(TARGETARCH) \
 	--build=$(BUILDARCH) \
 	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache
 CC := "${CC} "
-LIBS := "-lrt "

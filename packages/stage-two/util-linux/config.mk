d8bced13770d4ff0eecf10225a04851f31fe4222
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-03 12:27:33 +0300

    stage-two/util-linux: upgrade to version 2.31
    - use ncurses wide-character libs
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/util-linux/config.mk
+++ packages/stage-two/util-linux/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := util-linux
-PKGVER := 2.23.2
+PKGVER := 2.31
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://www.kernel.org/pub/linux/utils/util-linux/v$(PKGVER)/$(PKGSRC)
 COPTS := --enable-partx \
 	--enable-write \
 	--disable-wall \
@@ -12,6 +13,6 @@ COPTS := --enable-partx \
 	--host=$(TARGETARCH) \
 	--disable-nls \
 	--enable-raw \
-	--with-ncurses \
+	--with-ncursesw \
 	--disable-makeinstall-chown
 CC := "${CC} "

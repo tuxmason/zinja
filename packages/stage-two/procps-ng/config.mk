7de401a8d8ed257efe1a621d859977dcdfa8e631
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-03 13:08:13 +0300

    stage-two/procps-ng: upgrade to version 3.3.12
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/procps-ng/config.mk
+++ packages/stage-two/procps-ng/config.mk
@@ -1,23 +1,21 @@
 PKGNAME := procps-ng
-PKGVER := 3.3.9
+PKGVER := 3.3.12
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://sourceforge.net/projects/procps-ng/files/Production/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--bindir=/bin \
 	--sbindir=/sbin \
 	--libdir=/usr/lib \
 	--disable-kill \
-	--disable-skill \
 	--exec-prefix= \
 	--mandir=/usr/share/man \
 	--docdir=/usr/share/doc \
-	--disable-w-from \
 	--with-ncurses \
 	--disable-rpath \
-	--disable-static \
 	--sysconfdir=/etc \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \

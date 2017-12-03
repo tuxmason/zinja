1386979423daa7f1cf5bf6750a94efcd56a62371
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-03 20:16:05 +0300

    stage-two/flex: upgrade to version 2.6.4
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/flex/config.mk
+++ packages/stage-two/flex/config.mk
@@ -1,12 +1,12 @@
 PKGNAME := flex
-PKGVER := 2.5.37
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGVER := 2.6.4
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://github.com/westes/flex/releases/download/v$(PKGVER)/$(PKGSRC)
 COPTS := --prefix=/usr \
-	--disable-rpath \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \
         --cache-file=$(PKGOBJDIR)/$(PKGNAME).cache

6eda10dcba2712ecee5bdb7371a9280a79e6ff3e
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-05 01:12:25 +0300

    stage-two/curl: upgrade to version 7.57.0
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/curl/config.mk
+++ packages/stage-two/curl/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := curl
-PKGVER := 7.45.0
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGVER := 7.57.0
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://curl.haxx.se/download/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \

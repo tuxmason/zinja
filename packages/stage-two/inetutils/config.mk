34fe99fe9bca12bee373df4c035974e14d31e3f2
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 20:22:10 +0300

    stage-two/inetutils: upgrade to version 1.9.4
    - add SRCURL var
    - remove obsolete patches
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/inetutils/config.mk
+++ packages/stage-two/inetutils/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := inetutils
-PKGVER := 1.9.1
+PKGVER := 1.9.4
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--host=$(TARGETARCH) \
 	--build=$(BUILDARCH) \
@@ -14,5 +15,9 @@ COPTS := --prefix=/usr \
 	--disable-ifconfig     \
 	--disable-syslogd      \
 	--disable-whois        \
+	--disable-rsh \
+	--disable-rcp \
+	--disable-rexec \
+	--disable-rlogin \
 	--disable-servers
 CC := "${CC} "

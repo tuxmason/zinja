082c37f6bbd9dcf1b7e6054485814e2f5144b9e7
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 21:30:45 +0300

    stage-two/make: upgrade to version 4.2.1
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/make/config.mk
+++ packages/stage-two/make/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := make
-PKGVER := 4.0
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGVER := 4.2.1
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--without-guile \
 	--build=$(BUILDARCH) \

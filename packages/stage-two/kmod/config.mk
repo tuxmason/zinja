a7110e49e2a3fa350dfd97919997025f75d38f21
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 21:48:34 +0300

    stage-two/kmod: upgrade to version 24
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/kmod/config.mk
+++ packages/stage-two/kmod/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := kmod
-PKGVER := 16
+PKGVER := 24
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://www.kernel.org/pub/linux/utils/kernel/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--bindir=/sbin \
 	--libdir=/lib \

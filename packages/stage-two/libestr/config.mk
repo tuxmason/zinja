ad55817e6dee87b1263f5c36152c4913bdb726c2
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 22:12:57 +0300

    stage-two/libestr: upgrade to version 0.1.10
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libestr/config.mk
+++ packages/stage-two/libestr/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := libestr
-PKGVER := 0.1.9
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGVER := 0.1.10
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://libestr.adiscon.com/files/download/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \

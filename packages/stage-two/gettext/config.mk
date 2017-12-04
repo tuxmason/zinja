00afb9bed95bcc4ac582dbc118042dccfd00f53f
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 18:55:42 +0300

    stage-two/gettext: upgrade to version 0.19.8.1
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/gettext/config.mk
+++ packages/stage-two/gettext/config.mk
@@ -1,16 +1,20 @@
 PKGNAME := gettext
-PKGVER := 0.18.3.1
+PKGVER := 0.19.8.1
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--disable-java \
 	--without-csharp \
+	--disable-rpath \
+	--enable-threads=posix \
 	--host=$(TARGETARCH) \
 	--build=$(BUILDARCH) \
+	--with-sysroot=$(SYSROOTDIR) \
 	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache
 CC := "${CC} "
 CXX := "${CXX} "
-LIBS := "-lrt -lpthread"
+LIBS := "-lrt -lpthread -ldl -lm"

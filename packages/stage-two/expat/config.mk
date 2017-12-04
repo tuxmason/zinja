3dc260314cb5c4f76c848e7ba44e04d084b92bfd
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 23:53:07 +0300

    stage-two/expat: upgrade to version 2.2.5
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/expat/config.mk
+++ packages/stage-two/expat/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := expat
-PKGVER := 2.1.0
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGVER := 2.2.5
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://prdownloads.sourceforge.net/expat/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--localstatedir=/var \
 	--build=$(BUILDARCH) \

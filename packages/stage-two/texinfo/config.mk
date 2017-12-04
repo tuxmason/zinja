7766b6f6ce2f0e79ce81cbaf898379b849d18d13
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 23:16:15 +0300

    stage-two/texinfo: upgrade to version 6.5
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/texinfo/config.mk
+++ packages/stage-two/texinfo/config.mk
@@ -1,13 +1,13 @@
 PKGNAME := texinfo
-PKGVER := 5.2
+PKGVER := 6.5
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr  \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
 CC := "${CC} "
 CXX := "${CXX} "
-LIBS := "-lpthread"

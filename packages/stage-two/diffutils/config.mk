6e3c74b08b8380dced7384d340f845a86306a517
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 12:43:21 +0300

    stage-two/diffutils: upgrade to version 3.6
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/diffutils/config.mk
+++ packages/stage-two/diffutils/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := diffutils
-PKGVER := 3.3
+PKGVER := 3.6
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)

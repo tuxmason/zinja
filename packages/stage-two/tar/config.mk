b688c01c31ae994fbd278faa4cfb4928a20019c9
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 22:58:25 +0300

    stage-two/tar: upgrade to version 1.29
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/tar/config.mk
+++ packages/stage-two/tar/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := tar
-PKGVER := 1.27.1
+PKGVER := 1.29
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--bindir=/bin \
 	--build=$(BUILDARCH) \

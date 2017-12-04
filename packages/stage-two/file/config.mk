f4fd2ae9916aab6817bf9b820a945bb28e4a62fe
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 12:47:12 +0300

    stage-two/file: upgrade to version 5.32
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/file/config.mk
+++ packages/stage-two/file/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := file
-PKGVER := 5.15
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGVER := 5.32
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := ftp://ftp.astron.com/pub/file/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)

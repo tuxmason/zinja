bc999e02ad75a0afc69de51bb5a09dd8de74985e
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-05 00:56:54 +0300

    stage-two/openssl: upgrade to version 1.1.0g
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/openssl/config.mk
+++ packages/stage-two/openssl/config.mk
@@ -1,12 +1,14 @@
 PKGNAME := openssl
-PKGVER := 1.0.2d
+PKGVER := 1.1.0g
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://openssl.org/source/$(PKGSRC)
 COPTS := shared \
-	linux-generic32 \
+	-DOPENSSL_NO_ASYNC \
+	linux-x86_64 \
 	zlib-dynamic \
 	--openssldir=/etc/ssl \
 	--prefix=/usr \

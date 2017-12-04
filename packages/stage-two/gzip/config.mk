c81408c7dca7382420dcbd7475e3aab0f76e1bbe
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 20:07:33 +0300

    stage-two/gzip: upgrade to version 1.8
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/gzip/config.mk
+++ packages/stage-two/gzip/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := gzip
-PKGVER := 1.6
+PKGVER := 1.8
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--bindir=/bin \
 	--build=$(BUILDARCH) \

e4c1301c62238e138fb3e50831966c4445a57b6f
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 22:24:37 +0300

    stage-two/libee: upgrade to version 0.4.1
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libee/config.mk
+++ packages/stage-two/libee/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := libee
 PKGVER := 0.4.1
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://www.libee.org/files/download/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \

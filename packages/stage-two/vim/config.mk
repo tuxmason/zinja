33d39ecb812b55b5bff1274e842630c357f38467
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 23:32:30 +0300

    stage-two/vim: upgrade to version 8.0.586
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/vim/config.mk
+++ packages/stage-two/vim/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := vim
-PKGVER :=7.4
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
-PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)74
+PKGVER := 8.0.586
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)80
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := ftp://ftp.vim.org/pub/vim/unix/$(PKGSRC)
 COPTS := --prefix=/usr \
         --build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \

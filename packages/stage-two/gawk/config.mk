908c7a50d9c6e554c81d557274fe9e3800f1e279
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 13:03:51 +0300

    stage-two/gawk: upgrade to version 4.2.0
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/gawk/config.mk
+++ packages/stage-two/gawk/config.mk
@@ -1,11 +1,13 @@
 PKGNAME := gawk
-PKGVER := 4.1.0
+PKGVER := 4.2.0
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
 	--libexecdir=/usr/lib \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)

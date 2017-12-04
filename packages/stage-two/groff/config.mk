eb3a7534524f6f7ac3ed4a6304f001ecece51f87
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 20:01:23 +0300

    stage-two/groff: upgrade to version 1.22.3
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/groff/config.mk
+++ packages/stage-two/groff/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := groff
-PKGVER := 1.22.2
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGVER := 1.22.3
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--with-x=no \
 	--build=$(BUILDARCH) \

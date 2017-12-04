4592dcac889a21c0341021ce1add1c72f2c25e33
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 21:52:07 +0300

    stage-two/patch: upgrade to version 2.7.5
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/patch/config.mk
+++ packages/stage-two/patch/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := patch
-PKGVER := 2.7.1
+PKGVER := 2.7.5
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \

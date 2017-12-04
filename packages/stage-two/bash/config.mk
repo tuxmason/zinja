6bbaf8c6f241ebcc4ae9171ebe25df19aa202aa1
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 11:48:43 +0300

    stage-two/bash: upgrade to version 4.4.12
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/bash/config.mk
+++ packages/stage-two/bash/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := bash
-PKGVER := 4.2
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGVER := 4.4.12
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--bindir=/bin \
 	--disable-nls \

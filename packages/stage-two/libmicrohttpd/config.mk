40a786e95f72e84ece168f3de5ad0156435c588d
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 23:47:19 +0300

    stage-two/libmicrohttpd: upgrade to version 0.9.57
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libmicrohttpd/config.mk
+++ packages/stage-two/libmicrohttpd/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := libmicrohttpd
-PKGVER := 0.9.38
+PKGVER := 0.9.57
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)

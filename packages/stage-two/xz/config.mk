8b83ff3cff72d3ffccece04617c3dec21d8608dc
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 21:37:28 +0300

    stage-two/xz: upgrade to version 5.2.3
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/xz/config.mk
+++ packages/stage-two/xz/config.mk
@@ -1,12 +1,12 @@
 PKGNAME := xz
-PKGVER := 5.0.5
+PKGVER := 5.2.3
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://tukaani.org/xz/$(PKGSRC)
 COPTS := --prefix=/usr \
-	--disable-static \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
 

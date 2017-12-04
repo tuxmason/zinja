031adc83555bf059f082b54bbe073440c73e258d
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 13:10:35 +0300

    stage-two/findutils: upgrade to version 4.6.0
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/findutils/config.mk
+++ packages/stage-two/findutils/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := findutils
-PKGVER := 4.5.12
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGVER := 4.6.0
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \

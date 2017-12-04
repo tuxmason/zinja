d6f32a2606a53da10af9a91b82c3dea7d1eb7ce1
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 19:47:34 +0300

    stage-two/grep: upgrade to version 3.1
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/grep/config.mk
+++ packages/stage-two/grep/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := grep
-PKGVER :=2.15
+PKGVER :=3.1
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--bindir=/bin \
 	--disable-nls \

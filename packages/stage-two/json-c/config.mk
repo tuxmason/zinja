9aa6f40bde00c6375a674f02900a0bca7c77be9b
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 22:52:04 +0300

    stage-two/json-c: upgrade to version 0.12.1
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/json-c/config.mk
+++ packages/stage-two/json-c/config.mk
@@ -1,11 +1,13 @@
 PKGNAME := json-c
-PKGVER := 0.11
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGVER := 0.12.1
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://s3.amazonaws.com/json-c_releases/releases/$(PKGSRC)
 COPTS := --prefix=/usr \
+	--enable-rdrand \
 	--enable-shared \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \

b1dd50b18007db62fc9fa690ee961255975ecadb
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 09:58:32 +0300

    stage-two/automake: upgrade to version 1.15.1
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/automake/config.mk
+++ packages/stage-two/automake/config.mk
@@ -1,10 +1,11 @@
 PKGNAME := automake
-PKGVER := 1.14
+PKGVER := 1.15.1
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)

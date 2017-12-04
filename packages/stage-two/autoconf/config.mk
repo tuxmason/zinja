4d11883bec4db89e73633e765dde1213b57a3b05
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-04 09:49:05 +0300

    stage-two/autoconf: add musl lib C support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/autoconf/config.mk
+++ packages/stage-two/autoconf/config.mk
@@ -5,6 +5,7 @@ PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)

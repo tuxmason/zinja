d1eea0d6384229114c9129dace56bd8fb2bbc7a1
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-07 10:27:16 +0300

    packages/stage-two: add nss version 3.34.1
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/nss/config.mk
@@ -0,0 +1,12 @@
+PKGNAME := nss
+PKGVER := 3.34.1
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://archive.mozilla.org/pub/security/nss/releases/NSS_3_34_1_RTM/src/$(PKGSRC)
+COPTS := 
+CC := "${CC} "
+CXX := "${CXX} "
+HOST_CC := gcc

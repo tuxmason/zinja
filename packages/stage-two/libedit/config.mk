7be672689d2415ff65f26591bdf870b4273d11cd
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-07 14:36:52 +0300

    stage-two/libedit: upgrade to version 20170329-3.1
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libedit/config.mk
+++ packages/stage-two/libedit/config.mk
@@ -1,12 +1,12 @@
 PKGNAME := libedit
-PKGVER := 20150325-3.1
+PKGVER := 20170329-3.1
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://thrysoee.dk/editline/$(PKGSRC)
 COPTS := --prefix=/usr \
-	--enable-widec \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
 

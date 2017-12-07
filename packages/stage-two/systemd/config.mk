e4837824c79d74db4479b2f0401176dd71347c33
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2017-12-07 10:36:42 +0300

    stage-two/systemd: upgrade to version 234
    - add SRCURL var
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/systemd/config.mk
+++ packages/stage-two/systemd/config.mk
@@ -1,48 +1,50 @@
 PKGNAME := systemd
-PKGVER := 216
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGVER := 234
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
-PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
-PATCHDIR := $(PATCHDB)/$(PKGNAME)
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/musl/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)/musl
+SRCURL := https://github.com/systemd/systemd/archive/v234.tar.gz
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \
 	--sysconfdir=/etc \
 	--localstatedir=/var \
 	--libexecdir=/usr/lib \
-	--docdir=/usr/share/doc/systemd-216 \
+	--docdir=/usr/share/doc/systemd-234 \
 	--with-rootprefix="" \
 	--with-rootlibdir=/lib \
 	--enable-split-usr \
-	--disable-gudev \
 	--disable-efi \
+	 --disable-lto \
 	--disable-gnutls \
 	--disable-libcryptsetup \
 	--disable-gcrypt \
 	--without-python \
-	--disable-readahead \
 	--disable-manpages \
 	--enable-static=no \
 	--without-python \
-	--disable-myhostname \
-	--disable-machined \
-	--disable-resolved \
 	--disable-sysusers \
 	--disable-tests \
-	--disable-localed \
 	--disable-selinux \
 	--disable-firstboot \
 	--disable-ldconfig \
-	--disable-logind \
 	--disable-coredump \
-	--enable-introspection=no \
+	--disable-audit \
+	--disable-smack \
+	--disable-ima \
+	--disable-myhostname \
+	--disable-nss-systemd \
+	--disable-resolved \
+	--with-fallback-hostname=mugenjin \
+	--with-default-dnssec=no \
 	--with-kbd-loadkeys=/bin/loadkeys \
 	--with-kbd-setfont=/bin/setfont \
 	--with-dbuspolicydir=/etc/dbus-1/system.d \
-	--with-dbusinterfacedir=/usr/share/dbus-1/interfaces \
 	--with-dbussessionservicedir=/usr/share/dbus-1/services \
 	--with-dbussystemservicedir=/usr/share/dbus-1/system-services \
 	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache
-
-KMOD_LIBS = -lkmod
+CPPFLAGS := "-I$(SYSROOTDIR)/usr/include/nspr -I$(SYSROOTDIR)/usr/include/nss"
+CFLAGS := "-D_GNU_SOURCE -DNO_LIBC_FTW"
+KMOD_LIBS := -lkmod

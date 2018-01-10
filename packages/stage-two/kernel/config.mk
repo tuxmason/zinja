6a33f8a7a0acc1800d57ee6b7ff949081a16175c
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-10 13:24:53 +0300

    stage-two/kernel: update build rules
    - add ARM support
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/kernel/config.mk
+++ packages/stage-two/kernel/config.mk
@@ -4,6 +4,13 @@ PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PATCHLIST := $(PATCHDB)/kernel/list.txt
 PATCHDIR := $(PATCHDB)/kernel
-CONF := $(PATCHDB)/kernel/kconfig.x86_64
+ifeq ($(ARCH),arm)
+	CONF := $(PATCHDB)/kernel/kconfig.arm
+	KIMAGE := zImage
+endif
+ifeq ($(ARCH),x86_64)
+	CONF := $(PATCHDB)/kernel/kconfig.x86_64
+	KIMAGE := bzImage
+endif
 PKGOBJDIR := $(TCBUILDROOT)/kernel-obj
 SRCURL := https://cdn.kernel.org/pub/linux/kernel/v4.x/$(PKGSRC)

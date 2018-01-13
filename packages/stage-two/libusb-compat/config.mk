808c8f2a8370ea4e14774d15d4fb191d7d568561
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-13 21:43:19 +0300

    stage-two/libusb-compat: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/libusb-compat/config.mk
@@ -0,0 +1,15 @@
+PKGNAME := libusb-compat
+PKGVER := 0.1.5
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://downloads.sourceforge.net/libusb/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "

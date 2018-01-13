0b5f1931a9bfb5718214a105c843c3b6c65f5965
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-13 22:48:27 +0300

    stage-two/libnetfilter_conntrack: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/libnetfilter_conntrack/config.mk
@@ -0,0 +1,16 @@
+PKGNAME := libnetfilter_conntrack
+PKGVER := 1.0.6
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://netfilter.org/projects/$(PKGNAME)/files/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "

2ebfb6b9dd79d3791a5cddac0b8b1c07704e7143
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-13 22:27:02 +0300

    stage-two/libnftnl: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/libnftnl/config.mk
@@ -0,0 +1,17 @@
+PKGNAME := libnftnl
+PKGVER := 1.0.9
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://netfilter.org/projects/$(PKGNAME)/files/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--with-json-parsing
+
+CC := "${CC} "
+CXX := "${CXX} "

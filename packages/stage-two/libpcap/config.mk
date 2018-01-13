134f3e2afc28c327945a1d91fe4d90ce0876970c
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-13 22:18:59 +0300

    stage-two/libpcap: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/libpcap/config.mk
@@ -0,0 +1,18 @@
+PKGNAME := libpcap
+PKGVER := 1.8.1
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://www.tcpdump.org/release/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--enable-ipv6 \
+	--sysconfdir=/etc \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--with-pcap="linux"
+
+CC := "${CC} "
+CXX := "${CXX} "
+CFLAGS := "-I$(SYSROOTDIR)/usr/include/libnl3"

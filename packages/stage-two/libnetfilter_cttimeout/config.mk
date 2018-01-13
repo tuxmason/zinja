cd1dc6621af6e360b8ef80cba1c42c57c34bf4d1
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-13 22:50:43 +0300

    stage-two/libnetfilter_cttimeout: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/libnetfilter_cttimeout/config.mk
@@ -0,0 +1,17 @@
+PKGNAME := libnetfilter_cttimeout
+PKGVER := 1.0.0
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://netfilter.org/projects/$(PKGNAME)/files/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "
+LDFLAGS := "-L$(SYSROOTDIR)/usr/lib -L$(SYSROOTDIR)/usr/lib"

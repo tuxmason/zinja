6e6e934407e332231235e56216cf704b5103c90d
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-13 22:15:31 +0300

    stage-two/bluez: initial commit
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/bluez/config.mk
@@ -0,0 +1,20 @@
+PKGNAME := bluez
+PKGVER := 5.48
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://www.kernel.org/pub/linux/bluetooth/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--enable-library \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+
+CC := "${CC} "
+CXX := "${CXX} "
+CPPFLAGS := "-I$(SYSROOTDIR)/usr/include/glib-2.0 \
+	-I$(SYSROOTDIR)/usr/lib/glib-2.0/include"
+LDFLAGS := "-L$(SYSROOTDIR)/lib -L$(SYSROOTDIR)/usr/lib"

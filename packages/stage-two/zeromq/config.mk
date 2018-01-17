076e2054f8cf28082c087ff4972b99ea52b72a19
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-17 22:26:01 +0300

    stage-two/zeromq: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/zeromq/config.mk
@@ -0,0 +1,14 @@
+PKGNAME := zeromq
+PKGVER := 4.2.3
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://github.com/zeromq/libzmq/releases/download/v4.2.3/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "

787547d2f920ec6dec39f035d63ceb80fc54886f
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-17 22:41:38 +0300

    stage-two/lzip: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/lzip/config.mk
@@ -0,0 +1,13 @@
+PKGNAME := lzip
+PKGVER := 1.19
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://download.savannah.gnu.org/releases/$(PKGNAME)/$(PKGSRC)
+COPTS := --prefix=/usr
+
+CC := "${CC} "
+CXX := "${CXX} "
+LDFLAGS := "-L$(SYSROOTDIR)/usr/lib -L$(SYSROOTDIR)/usr/lib"

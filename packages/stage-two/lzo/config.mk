10a46e41b4c4d8ba595323ae636c7a81150cfb17
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-17 22:44:58 +0300

    stage-two/lzo: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/lzo/config.mk
@@ -0,0 +1,16 @@
+PKGNAME := lzo
+PKGVER := 2.10
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://www.oberhumer.com/opensource/lzo/download/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--enable-shared \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--docdir=/usr/share/doc/$(PKGNAME)-$(PKGVER)
+
+CC := "${CC} "
+CXX := "${CXX} "

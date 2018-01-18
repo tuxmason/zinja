787b6891152defa15afb931b270f76914bc03b56
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-19 00:24:02 +0300

    stage-two/grub: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/grub/config.mk
@@ -0,0 +1,16 @@
+PKGNAME := grub
+PKGVER := 2.02
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--sysconfdir=/etc \
+	--disable-werror
+
+CC := "${CC} "
+CXX := "${CXX} "

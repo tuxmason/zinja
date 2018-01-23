62cc3d6d35c9c937930b949c7e3069087c60b1bb
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-23 13:59:35 +0300

    stage-two/cyrus-sasl: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/cyrus-sasl/config.mk
@@ -0,0 +1,21 @@
+PKGNAME := cyrus-sasl
+PKGVER := 2.1.26
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := ftp://ftp.cyrusimap.org/$(PKGNAME)/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--enable-login \
+	--sysconfdir=/etc \
+	--with-dblib=gdbm \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--enable-auth-sasldb \
+	--with-dbpath=/var/lib/sasl/sasldb2 \
+	--with-saslauthd=/var/run/saslauthd
+
+CC := "${CC} "
+CXX := "${CXX} "
+HOST_CC := gcc

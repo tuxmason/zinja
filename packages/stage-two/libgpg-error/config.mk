5b348fb47b38d9814fd50ac43526d4cc74620a63
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-23 12:34:13 +0300

    stage-two/libgpg-error: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/libgpg-error/config.mk
@@ -0,0 +1,17 @@
+PKGNAME := libgpg-error
+PKGVER := 1.27
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://www.gnupg.org/ftp/gcrypt/$(PKGNAME)/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--disable-tests \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "

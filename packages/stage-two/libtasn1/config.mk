616096d4c7790066c2ac683ca714e9d0909f1eb0
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-23 12:22:08 +0300

    stage-two/libtasn1: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/libtasn1/config.mk
@@ -0,0 +1,14 @@
+PKGNAME := libtasn1
+PKGVER := 4.13
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "

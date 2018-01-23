3cc725a4e03ba5b8f1b9847553f282b37b311f19
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-23 14:10:27 +0300

    stage-two/gnupg: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/gnupg/config.mk
@@ -0,0 +1,26 @@
+PKGNAME := gnupg
+PKGVER := 2.2.4
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://www.gnupg.org/ftp/gcrypt/$(PKGNAME)/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--enable-g13 \
+	--sysconfdir=/etc \
+	--with-capabilities \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--enable-symcryptrun \
+	--enable-maintainer-mode \
+	--with-ksba-prefix=$(SYSROOTDIR)/usr \
+	--with-npth-prefix=$(SYSROOTDIR)/usr \
+	--with-libgcrypt-prefix=$(SYSROOTDIR)/usr \
+	--with-gpg-error-prefix=$(SYSROOTDIR)/usr \
+	--with-libassuan-prefix=$(SYSROOTDIR)/usr \
+	--docdir=/usr/share/doc/$(PKGNAME)-$(PKGVER)
+
+CC := "${CC} "
+CXX := "${CXX} "

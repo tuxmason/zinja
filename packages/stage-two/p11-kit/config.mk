8be06ac9604d092ffd44c1243320282689cb38a5
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-23 13:43:11 +0300

    stage-two/p11-kit: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/p11-kit/config.mk
@@ -0,0 +1,16 @@
+PKGNAME := p11-kit
+PKGVER := 0.23.9
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://github.com/p11-glue/p11-kit/releases/download/$(PKGVER)/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--with-trust-paths=/etc/pki/anchors
+
+CC := "${CC} "
+CXX := "${CXX} "

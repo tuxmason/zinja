9b54ae689d66611414240503e7fe5c748190d654
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-23 12:04:19 +0300

    stage-two/libatomic_ops: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/libatomic_ops/config.mk
@@ -0,0 +1,17 @@
+PKGNAME := libatomic_ops
+PKGVER := 7.6.2
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://github.com/ivmai/libatomic_ops/releases/download/v7.6.2/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--docdir=/usr/share/doc/$(PKGNAME)-$(PKGVER)
+
+CC := "${CC} "
+CXX := "${CXX} "

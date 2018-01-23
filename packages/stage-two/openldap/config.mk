ca9b60cf58a6dbc820f148b432e8b46ba7558812
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-23 14:03:24 +0300

    stage-two/openldap: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/openldap/config.mk
@@ -0,0 +1,33 @@
+PKGNAME := openldap
+PKGVER := 2.4.45
+PKGSRC := $(PKGNAME)-$(PKGVER).tgz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--enable-bdb=no \
+	--enable-perl=no \
+	--enable-ndb=no \
+	--enable-hdb=no \
+	--enable-crypt \
+	--disable-debug \
+	--enable-spasswd \
+	--enable-modules \
+	--enable-dynamic \
+	--with-cyrus-sasl \
+	--enable-overlays=mod \
+	--enable-rlookups \
+	--enable-backends=mod \
+	--sysconfdir=/etc \
+	--build=$(BUILDARCH) \
+	--libexecdir=/usr/lib \
+	--host=$(TARGETARCH) \
+	--with-yielding-select=yes \
+	ac_cv_func_memcmp_working=yes
+
+CC := "${CC} "
+CXX := "${CXX} "
+LDFLAGS := "-L$(SYSROOTDIR)/lib -L$(SYSROOTDIR)/usr/lib"
+CFLAGS := "-D_GNU_SOURCE -fPIC -I$(SYSROOTDIR)/usr/include -I$(SYSROOTDIR)/usr/include/openssl"

dfe512410f1c1428cc2a1a08f5f0c104d50e5638
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-26 01:53:52 +0300

    xorg/libs: Xorg Libraries - initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>
    (cherry picked from commit cea89404367400606eb950e1f95c1493cbf82958)

---
+++ packages/stage-two/xorg/libs/libXrandr/config.mk
@@ -0,0 +1,17 @@
+PKGNAME := libXrandr
+PKGVER := 1.5.1
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://www.x.org/pub/individual/lib/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--disable-malloc0returnsnull
+
+CC := "${CC} "
+CXX := "${CXX} "
---
+++ packages/stage-two/xorg/libs/libXrender/config.mk
@@ -0,0 +1,17 @@
+PKGNAME := libXrender
+PKGVER := 0.9.10
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://www.x.org/pub/individual/lib/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--disable-malloc0returnsnull
+
+CC := "${CC} "
+CXX := "${CXX} "

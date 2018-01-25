6193b309e9180b83d84b3b8dfef3cf5f67c37d50
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-25 20:42:29 +0300

    stage-two/libpng: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/libpng/config.mk
@@ -0,0 +1,14 @@
+PKGNAME := libpng
+PKGVER := 1.6.34
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://downloads.sourceforge.net/$(PKGNAME)/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "

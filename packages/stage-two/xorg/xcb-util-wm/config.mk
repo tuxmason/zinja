46c77b383e66b4043b7bdea1b5d59c3e96eae00c
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-26 09:22:20 +0300

    xorg/xcb-util-wm: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/xorg/xcb-util-wm/config.mk
@@ -0,0 +1,16 @@
+PKGNAME := xcb-util-wm
+PKGVER := 0.4.1
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://xcb.freedesktop.org/dist/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "

1dd42b95e3e786a086dae3e48251851aa5376b6e
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-26 09:53:47 +0300

    xorg/libomxil-bellagio: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/xorg/libomxil-bellagio/config.mk
@@ -0,0 +1,19 @@
+PKGNAME := libomxil-bellagio
+PKGVER := 0.9.3
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://sourceforge.net/projects/omxil/files/omxil/Bellagio%200.9.3/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--enable-shared \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--docdir=$(SYSROOTDIR)/usr/share/doc \
+	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache
+
+CC := "${CC} "
+CXX := "${CXX} "

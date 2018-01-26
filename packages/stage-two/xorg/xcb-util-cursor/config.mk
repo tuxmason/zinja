36d2043da7d5186625cc833276d0399e2b76f9ca
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-26 09:27:43 +0300

    xorg/xcb-util-cursor: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/xorg/xcb-util-cursor/config.mk
@@ -0,0 +1,16 @@
+PKGNAME := xcb-util-cursor
+PKGVER := 0.1.3
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://xcb.freedesktop.org/dist/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) 
+
+CC := "${CC} "
+CXX := "${CXX} "

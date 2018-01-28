40fa351e0ae33c1cba5cecb7b074be33d79ed2b1
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-28 03:22:26 +0300

    xorg/startup-notification: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>
    (cherry picked from commit fe270af70d3ebd1dd7a2a64fdd336cb1e487ab30)

---
+++ packages/stage-two/xorg/startup-notification/config.mk
@@ -0,0 +1,17 @@
+PKGNAME := startup-notification
+PKGVER := 0.12
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://www.freedesktop.org/software/startup-notification/releases/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache
+
+CC := "${CC} "
+CXX := "${CXX} "

06198eb148ae1319e8469150e4978997c35b79c7
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-28 05:06:23 +0300

    xorg/glu: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/xorg/glu/config.mk
@@ -0,0 +1,13 @@
+PKGNAME := glu
+PKGVER := 9.0.0
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) 
+
+CC := "${CC} "
+CXX := "${CXX} "

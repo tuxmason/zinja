39f1aa0046f70ba8383e16884182c5070324d013
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-28 09:57:05 +0300

    xorg/apps: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>
    (cherry picked from commit 904857733cab3c29d7276f345fd6ff34442602a7)

---
+++ packages/stage-two/xorg/apps/mkfontdir/config.mk
@@ -0,0 +1,16 @@
+PKGNAME := mkfontdir
+PKGVER := 1.0.7
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://www.x.org/pub/individual/app/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "
---
+++ packages/stage-two/xorg/apps/xclock/config.mk
@@ -0,0 +1,18 @@
+PKGNAME := xclock
+PKGVER := 1.0.7
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://www.x.org/pub/individual/app/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "
+PKG_CONFIG_SYSROOT_DIR := $(SYSROOTDIR)
+PKG_CONFIG_PATH := "$(SYSROOTDIR)/usr/lib/pkgconfig:$(SYSROOTDIR)/usr/share/pkgconfig"

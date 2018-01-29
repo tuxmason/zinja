bf0fe01e484a86cf6795e377cf67f04964a9e9b3
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-29 13:26:42 +0300

    xorg/fonts: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>
    (cherry picked from commit e4140bd44f463bb2ae7d8ca7a58296553a69dbc4)

---
+++ packages/stage-two/xorg/fonts/font-bh-ttf/config.mk
@@ -0,0 +1,18 @@
+PKGNAME := font-bh-ttf
+PKGVER := 1.0.3
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://www.x.org/pub/individual/font/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "
+
+MAPFILES_PATH := "$(SYSROOTDIR)/usr/share/fonts/X11/util"

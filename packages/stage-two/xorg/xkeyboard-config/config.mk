acfe664eeecf3cd82cfd4797d0006c4c82b6ac7b
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-29 13:31:47 +0300

    xorg/xkeyboard-config: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>
    (cherry picked from commit a3a5689e58d80ef833da8d67590b09d43411d784)

---
+++ packages/stage-two/xorg/xkeyboard-config/config.mk
@@ -0,0 +1,18 @@
+PKGNAME := xkeyboard-config
+PKGVER := 2.22
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://www.x.org/pub/individual/data/xkeyboard-config/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--disable-runtime-deps \
+	--with-xkb-rules-symlink=xorg
+
+CC := "${CC} "
+CXX := "${CXX} "

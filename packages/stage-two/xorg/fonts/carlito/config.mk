f1da390797832687e822ee931d77990f023efa3f
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-31 20:52:31 +0300

    xorg/fonts/carlito: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>
    (cherry picked from commit 20b67cb3ec762bceb594d11e66fcbe921cfcc435)

---
+++ packages/stage-two/xorg/fonts/carlito/config.mk
@@ -0,0 +1,8 @@
+PKGNAME := crosextrafonts-carlito
+PKGVER := 20130920
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://gsdview.appspot.com/chromeos-localmirror/distfiles/$(PKGSRC)

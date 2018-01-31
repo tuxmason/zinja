7ff78c2011a121b966b3444b4422099ce8d55018
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-31 23:56:32 +0300

    stage-two/libgudev: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/libgudev/config.mk
@@ -0,0 +1,17 @@
+PKGNAME := libgudev
+PKGVER := 232
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnome.org/pub/gnome/sources/$(PKGNAME)/$(PKGVER)/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--disable-umockdev
+
+CC := "${CC} "
+CXX := "${CXX} "
+PKG_CONFIG_SYSROOT_DIR := $(SYSROOTDIR)
+PKG_CONFIG_PATH := "$(SYSROOTDIR)/usr/lib/pkgconfig:$(SYSROOTDIR)/usr/share/pkgconfig"

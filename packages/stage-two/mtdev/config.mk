b9455a086f04b66767fe4eb3ec9dbf3d504f0d53
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-02-01 00:02:40 +0300

    stage-two/mtdev: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>
    (cherry picked from commit 945b7fd048a421a40ddb84b8a00a75d2291e6a4e)

---
+++ packages/stage-two/mtdev/config.mk
@@ -0,0 +1,14 @@
+PKGNAME := mtdev
+PKGVER := 1.1.5
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://bitmath.org/code/$(PKGNAME)/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "

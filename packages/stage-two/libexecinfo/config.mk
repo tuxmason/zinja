2a01bf11bec00cbc7a62b6b4e661c4a8210bbda7
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-31 23:54:23 +0300

    stage-two/libexecinfo: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/libexecinfo/config.mk
@@ -0,0 +1,15 @@
+PKGNAME := libexecinfo
+PKGVER := 1.1
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://ftp.nsysu.edu.tw/FreeBSD/ports/local-distfiles/itetcu/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "

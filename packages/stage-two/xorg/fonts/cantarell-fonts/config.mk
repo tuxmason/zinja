2790758743ac123fd44f232e4cd8072f33f67cca
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-31 20:51:45 +0300

    xorg/fonts/cantarell-fonts: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>
    (cherry picked from commit 22df4bbbdd32fc289e2daefe547e10f6fca34e28)

---
+++ packages/stage-two/xorg/fonts/cantarell-fonts/config.mk
@@ -0,0 +1,17 @@
+PKGNAME := cantarell-fonts
+PKGVER := 0.0.25
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnome.org/pub/gnome/sources/cantarell-fonts/0.0/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) \
+	--with-configdir=/etc/fonts/conf.avail
+
+CC := "${CC} "
+CXX := "${CXX} "

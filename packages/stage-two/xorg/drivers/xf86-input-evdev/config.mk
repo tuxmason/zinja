e57334164450933ae6b01d716038dd374beaba18
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-31 21:48:21 +0300

    xorg/drivers: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>
    (cherry picked from commit a68f4ba0412f02b268d898c4194caaa65d837a09)

---
+++ packages/stage-two/xorg/drivers/xf86-input-evdev/config.mk
@@ -0,0 +1,18 @@
+PKGNAME := xf86-input-libinput
+PKGVER := 0.26.0
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL :=  https://www.x.org/pub/individual/driver/$(PKGNAME)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH) 
+
+CC := "${CC} "
+CXX := "${CXX} "
+PKG_CONFIG_SYSROOT_DIR := $(SYSROOTDIR)
+PKG_CONFIG_PATH := "$(SYSROOTDIR)/usr/lib/pkgconfig:$(SYSROOTDIR)/usr/share/pkgconfig"

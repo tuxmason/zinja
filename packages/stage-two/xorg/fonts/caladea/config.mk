7ea7b7c2cc7ed5b55ba276557596a713836360b5
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-01-31 20:50:54 +0300

    xorg/fonts/caladea: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/xorg/fonts/caladea/config.mk
@@ -0,0 +1,8 @@
+PKGNAME := crosextrafonts
+PKGVER := 20130214
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://gsdview.appspot.com/chromeos-localmirror/distfiles/$(PKGSRC)

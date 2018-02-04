3c8b0799c613287f88e61f945add539018713370
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-02-04 14:55:26 +0300

    stage-two/libid3tag: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>
    (cherry picked from commit 1693988947bc1bef99abdb2a129ea80badd6e5ec)

---
+++ packages/stage-two/libid3tag/config.mk
@@ -0,0 +1,14 @@
+PKGNAME := libid3tag
+PKGVER := 0.15.1b
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://sourceforge.net/projects/mad/files/$(PKGNAME)/$(PKGVER)/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "

0feb069ad001f5f9de465c574330e41c09345749
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-02-04 14:51:04 +0300

    stage-two/giflib: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/giflib/config.mk
@@ -0,0 +1,14 @@
+PKGNAME := giflib
+PKGVER := 5.1.4
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://downloads.sourceforge.net/$(PKGNAME)/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "

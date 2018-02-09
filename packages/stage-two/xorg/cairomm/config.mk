789f233d4ec2a92c32a66dd6fb24cda3b1aa9565
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-02-09 13:22:30 +0300

    xorg/cairomm: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/xorg/cairomm/config.mk
@@ -0,0 +1,18 @@
+PKGNAME := cairomm
+PKGVER := 1.15.5
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://www.cairographics.org/releases/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "
+LDFLAGS := "-L$(SYSROOTDIR)/usr/lib -L$(SYSROOTDIR)/usr/lib"
+PKG_CONFIG_SYSROOT_DIR := $(SYSROOTDIR)
+PKG_CONFIG_PATH := "$(SYSROOTDIR)/usr/lib/pkgconfig:$(SYSROOTDIR)/usr/share/pkgconfig"

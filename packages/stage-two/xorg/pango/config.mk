7e6cac6d4d2210f958fc4fa1584c7ed736276a21
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-02-09 13:56:24 +0300

    xorg/pango: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/xorg/pango/config.mk
@@ -0,0 +1,22 @@
+PKGNAME := pango
+PKGVER := 1.41.0
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnome.org/pub/gnome/sources/$(PKGNAME)/1.14/$(PKGSRC) 
+COPTS := --prefix=/usr \
+	--with-xft \
+	--sysconfdir=/etc \
+	--disable-gtk-doc \
+	--enable-introspection=no \
+	--with-cairo \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} "
+CXX := "${CXX} "
+LDFLAGS := "-L$(SYSROOTDIR)/usr/lib -L$(SYSROOTDIR)/usr/lib"
+PKG_CONFIG_SYSROOT_DIR := $(SYSROOTDIR)
+PKG_CONFIG_PATH := "$(SYSROOTDIR)/usr/lib/pkgconfig:$(SYSROOTDIR)/usr/share/pkgconfig"

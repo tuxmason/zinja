2cf60efd12507a0917f8f5fee7c2f967163d6165
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-02-12 04:11:54 +0300

    stage-one/pl-mailsendmail: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-one/pl-mailsendmail/config.mk
@@ -0,0 +1,8 @@
+PKGNAME := Mail-Sendmail
+PKGVER := 0.80
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://search.cpan.org/CPAN/authors/id/N/NE/NEILB/$(PKGSRC)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-obj

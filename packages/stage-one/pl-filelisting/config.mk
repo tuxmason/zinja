fa0177e9c2ef8927b62f785b6106e56268481d86
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-02-12 17:35:15 +0300

    stage-one/pl-filelisting: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-one/pl-filelisting/config.mk
@@ -0,0 +1,8 @@
+PKGNAME := File-Listing
+PKGVER := 6.04
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://search.cpan.org/CPAN/authors/id/G/GA/GAAS/$(PKGSRC)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-obj

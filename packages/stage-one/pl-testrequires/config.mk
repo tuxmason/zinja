ebb7d7a8616e290f039cf05949455e270e065dff
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-02-12 04:40:41 +0300

    stage-one/pl-testrequires: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-one/pl-testrequires/config.mk
@@ -0,0 +1,8 @@
+PKGNAME := Test-Requires
+PKGVER := 0.10
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://search.cpan.org/CPAN/authors/id/T/TO/TOKUHIROM/Test-Requires-0.10.tar.gz
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-obj

714e8bc7926d37ff99b03ce5ca604f193d2d457a
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-02-12 04:47:48 +0300

    stage-one/pl-unicodelinebreak: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-one/pl-unicodelinebreak/config.mk
@@ -0,0 +1,8 @@
+PKGNAME := Unicode-LineBreak
+PKGVER := 2017.004
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://search.cpan.org/CPAN/authors/id/N/NE/NEZUMI/Unicode-LineBreak-2017.004.tar.gz
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-obj

ffdab8fbbfe714cba3688bc76ad99b374e59b7aa
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-02-12 03:58:06 +0300

    stage-one/pl-archivezip: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-one/pl-archivezip/config.mk
@@ -0,0 +1,8 @@
+PKGNAME := Archive-Zip
+PKGVER := 1.60
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://search.cpan.org/CPAN/authors/id/P/PH/PHRED/$(PKGSRC)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-obj

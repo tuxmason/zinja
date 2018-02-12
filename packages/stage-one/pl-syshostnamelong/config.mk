c92a29c51c773ded25561c73a4720a8eb4f3c40f
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-02-12 04:29:50 +0300

    stage-one/pl-syshostnamelong: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-one/pl-syshostnamelong/config.mk
@@ -0,0 +1,8 @@
+PKGNAME := Sys-Hostname-Long
+PKGVER := 1.5
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://search.cpan.org/CPAN/authors/id/S/SC/SCOTT/$(PKGSRC)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-obj

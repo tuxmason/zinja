8091af3c6ff556f458945f06c136591cf3b79526
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-02-12 04:42:39 +0300

    stage-one/pl-textcharwidth: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-one/pl-textcharwidth/config.mk
@@ -0,0 +1,8 @@
+PKGNAME := Text-CharWidth
+PKGVER := 0.04
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://search.cpan.org/CPAN/authors/id/K/KU/KUBOTA/$(PKGSRC)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-obj

6f698d07b6dfd89d8e3b7348d387eb79281f40fd
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-02-12 17:54:26 +0300

    stage-one/pl-libwww: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-one/pl-libwww/config.mk
@@ -0,0 +1,8 @@
+PKGNAME := libwww-perl
+PKGVER := 6.31
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://search.cpan.org/CPAN/authors/id/E/ET/ETHER/libwww-perl-6.31.tar.gz
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-obj

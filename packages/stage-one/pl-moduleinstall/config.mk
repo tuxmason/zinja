498ace794424362a2c9d5eb20253846a72e1acf8
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-02-12 04:16:30 +0300

    stage-one/pl-moduleinstall: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-one/pl-moduleinstall/config.mk
@@ -0,0 +1,8 @@
+PKGNAME := Module-Install
+PKGVER := 1.19
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://search.cpan.org/CPAN/authors/id/E/ET/ETHER/$(PKGSRC)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-obj

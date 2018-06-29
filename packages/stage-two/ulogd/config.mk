1d9f8cee403c74c2fa490885af1c58f0da847548
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-06-30 02:00:14 +0300

    stage-two/ulogd: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/ulogd/config.mk
+++ packages/stage-two/ulogd/config.mk
@@ -15,7 +15,14 @@ COPTS := --prefix=/usr \
 	--without-dbi \
 	--without-pgsql
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
 CFLAGS := "-D_GNU_SOURCE"
-LDFLAGS := "-L$(SYSROOTDIR)/usr/lib -L$(SYSROOTDIR)/usr/lib"
+LDFLAGS := "-L$(SYSROOTDIR)/lib -L$(SYSROOTDIR)/usr/lib"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

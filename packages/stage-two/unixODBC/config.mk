ff88541012073f76b500c116194a3fd2c265cf19
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-06-30 01:36:49 +0300

    stage-two/unixODBC: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/unixODBC/config.mk
+++ packages/stage-two/unixODBC/config.mk
@@ -7,6 +7,7 @@ PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := ftp://ftp.unixodbc.org/pub/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
+	--enable-static \
 	--enable-gui=no \
 	--enable-drivers \
 	--build=$(BUILDARCH) \
@@ -14,6 +15,13 @@ COPTS := --prefix=/usr \
 	--enable-drivers-conf \
 	--sysconfdir=/etc/unixODBC
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
 LDFLAGS := "-L$(SYSROOTDIR)/lib -L$(SYSROOTDIR)/usr/lib"
+
+PKGDIR := $(PKGDB)/unixodbc
+ORIGSRC := unixodbc_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

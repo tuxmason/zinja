f79776757a9227c774154bbc65841b67e5c46512
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 00:40:36 +0300

    stage-two/libgudev: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libgudev/config.mk
+++ packages/stage-two/libgudev/config.mk
@@ -7,11 +7,16 @@ PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := http://ftp.gnome.org/pub/gnome/sources/$(PKGNAME)/$(PKGVER)/$(PKGSRC)
 COPTS := --prefix=/usr \
+	--enable-static \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \
 	--disable-umockdev
 
-CC := "${CC} "
-CXX := "${CXX} "
-PKG_CONFIG_SYSROOT_DIR := $(SYSROOTDIR)
-PKG_CONFIG_PATH := "$(SYSROOTDIR)/usr/lib/pkgconfig:$(SYSROOTDIR)/usr/share/pkgconfig"
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

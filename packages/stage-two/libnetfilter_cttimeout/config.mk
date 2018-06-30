7ca988d70d5c33c84e36ca112febf3f6d86734d4
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-06-30 14:19:51 +0300

    stage-two/libnetfilter_cttimeout: add pkg mgt
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libnetfilter_cttimeout/config.mk
+++ packages/stage-two/libnetfilter_cttimeout/config.mk
@@ -1,17 +1,23 @@
-PKGNAME := libnetfilter_cttimeout
+PKGNAME := libnetfilter-cttimeout
 PKGVER := 1.0.0
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
-PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
-PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PKGSRC := libnetfilter_cttimeout-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/libnetfilter_cttimeout-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/libnetfilter_cttimeout-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
-SRCURL := http://netfilter.org/projects/$(PKGNAME)/files/$(PKGSRC)
+SRCURL := http://netfilter.org/projects/libnetfilter_cttimeout/files/$(PKGSRC)
 COPTS := --prefix=/usr \
+	--enable-static \
 	--sysconfdir=/etc \
 	--localstatedir=/var \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
 
-CC := "${CC} "
-CXX := "${CXX} "
-LDFLAGS := "-L$(SYSROOTDIR)/usr/lib -L$(SYSROOTDIR)/usr/lib"
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/libnetfilter_cttimeout-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

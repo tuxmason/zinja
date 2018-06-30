8d546687118bede9f9f15f804440c7bba710a61b
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 00:36:35 +0300

    stage-two/libidn2: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libidn2/config.mk
+++ packages/stage-two/libidn2/config.mk
@@ -13,5 +13,11 @@ COPTS := --prefix=/usr \
 	--host=$(TARGETARCH) \
 	--with-libunistring-prefix=$(SYSROOTDIR)/usr
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

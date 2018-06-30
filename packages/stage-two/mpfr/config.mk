3936cdb14c6cb6508dc20847dff2a6f3a92e0224
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-06-30 13:56:56 +0300

    stage-two/mpfr: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/mpfr/config.mk
+++ packages/stage-two/mpfr/config.mk
@@ -10,4 +10,12 @@ COPTS := --prefix=/usr \
 	--enable-shared \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
-CC := "${CC} "
+
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

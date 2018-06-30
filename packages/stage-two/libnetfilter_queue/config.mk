f6ca9a31b91ded60178ac2144d6bdb9e5ff29c35
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-06-30 14:18:29 +0300

    stage-two/libnetfilter_queue: add pkg mgt support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libnetfilter_queue/config.mk
+++ packages/stage-two/libnetfilter_queue/config.mk
@@ -1,16 +1,23 @@
-PKGNAME := libnetfilter_queue
+PKGNAME := libnetfilter-queue
 PKGVER := 1.0.3
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
-PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
-PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PKGSRC := libnetfilter_queue-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/libnetfilter_queue-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/libnetfilter_queue-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := http://netfilter.org/projects/$(PKGNAME)/files/$(PKGSRC)
 COPTS := --prefix=/usr \
+	--enable-static \
 	--sysconfdir=/etc \
 	--localstatedir=/var \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/libnetfilter_queue-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

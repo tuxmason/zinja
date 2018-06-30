e3d7dff9e6f7eee8a0ece3b1808a5a7529d6d663
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-06-30 14:24:51 +0300

    stage-two/libnetfilter_cthelper: add pkg mgt
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libnetfilter_cthelper/config.mk
+++ packages/stage-two/libnetfilter_cthelper/config.mk
@@ -1,17 +1,23 @@
-PKGNAME := libnetfilter_cthelper
+PKGNAME := libnetfilter-cthelper
 PKGVER := 1.0.0
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
-PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
-PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PKGSRC := libnetfilter_cthelper-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/libnetfilter_cthelper-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/libnetfilter_cthelper-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := http://netfilter.org/projects/$(PKGNAME)/files/$(PKGSRC)
 COPTS := --prefix=/usr \
+	--enable-static \
 	--sysconfdir=/etc \
 	--localstatedir=/var \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
 
-CC := "${CC} "
-CXX := "${CXX} "
-LDFLAGS := "-L$(SYSROOTDIR)/usr/lib -L$(SYSROOTDIR)/usr/lib"
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/libnetfilter_cthelper-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

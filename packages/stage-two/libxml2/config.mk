7e461cf55afe68f0cc9b10ca59d7ed20780ce3df
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-06-30 14:05:23 +0300

    stage-two/libxml2: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libxml2/config.mk
+++ packages/stage-two/libxml2/config.mk
@@ -7,10 +7,20 @@ PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := http://xmlsoft.org/sources/$(PKGSRC)
 COPTS := --prefix=/usr \
+	--with-icu \
+	--with-threads \
 	--with-history \
 	--without-python \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

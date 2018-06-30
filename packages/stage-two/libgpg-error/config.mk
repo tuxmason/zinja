20dd8efcfd943b0f2ea6df4a34f39469fb459822
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 00:41:13 +0300

    stage-two/libgpg-error: add pkg management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libgpg-error/config.mk
+++ packages/stage-two/libgpg-error/config.mk
@@ -7,11 +7,18 @@ PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := https://www.gnupg.org/ftp/gcrypt/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
+	--enable-static \
 	--disable-tests \
 	--sysconfdir=/etc \
 	--localstatedir=/var \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

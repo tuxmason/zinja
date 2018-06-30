6f882609cb21d908eff8c09ce9a593f0beb0ccc4
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-06-30 14:19:10 +0300

    stage-two/libnetfilter_log: add pkg mgt support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libnetfilter_log/config.mk
+++ packages/stage-two/libnetfilter_log/config.mk
@@ -1,17 +1,25 @@
-PKGNAME := libnetfilter_log
+PKGNAME := libnetfilter-log
 PKGVER := 1.0.1
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
-PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
-PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PKGSRC := libnetfilter_log-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/libnetfilter_log-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/libnetfilter_log-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
-PATCHDIR := $(PATCHDB)/$(PKGNAME)
+PATCHDIR := $(PATCHDB)/libnetfilter_log
 SRCURL := http://netfilter.org/projects/$(PKGNAME)/files/$(PKGSRC)
 COPTS := --prefix=/usr \
+	--enable-static \
 	--sysconfdir=/etc \
 	--localstatedir=/var \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
 
-CC := "${CC} "
-CXX := "${CXX} "
-LDFLAGS := "-L$(SYSROOTDIR)/usr/lib -L$(SYSROOTDIR)/usr/lib"
+CC := "${CC}"
+CXX := "${CXX}"
+
+LDFLAGS := "-L$(SYSROOTDIR)/lib -L$(SYSROOTDIR)/usr/lib"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/libnetfilter_log-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

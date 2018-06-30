377e1f0ede2a2de7ebb60b0c0b1bf98da3581023
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-06-30 14:00:41 +0300

    stage-two/lzo: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/lzo/config.mk
+++ packages/stage-two/lzo/config.mk
@@ -12,5 +12,11 @@ COPTS := --prefix=/usr \
 	--host=$(TARGETARCH) \
 	--docdir=/usr/share/doc/$(PKGNAME)-$(PKGVER)
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

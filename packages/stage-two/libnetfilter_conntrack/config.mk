dd4140ebd41201049bb9bdf15eea2cd76866042b
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-06-30 14:25:43 +0300

    stage-two/libnetfilter_conntrack: add package mgt
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libnetfilter_conntrack/config.mk
+++ packages/stage-two/libnetfilter_conntrack/config.mk
@@ -1,16 +1,23 @@
-PKGNAME := libnetfilter_conntrack
+PKGNAME := libnetfilter-conntrack
 PKGVER := 1.0.6
-PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
-PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
-PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PKGSRC := libnetfilter_conntrack-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/libnetfilter_conntrack-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/libnetfilter_conntrack-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := http://netfilter.org/projects/$(PKGNAME)/files/$(PKGSRC)
 COPTS := --prefix=/usr \
+	--enable-static \
 	--sysconfdir=/etc \
 	--localstatedir=/var \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/libnetfilter_conntrack-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

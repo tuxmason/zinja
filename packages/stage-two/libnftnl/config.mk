f2eb3f8f6fe12afd7213130ac4270a4f215ee926
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-06-30 14:15:09 +0300

    stage-two/libnftnl: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libnftnl/config.mk
+++ packages/stage-two/libnftnl/config.mk
@@ -7,11 +7,18 @@ PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := http://netfilter.org/projects/$(PKGNAME)/files/$(PKGSRC)
 COPTS := --prefix=/usr \
+	--enable-static \
 	--sysconfdir=/etc \
 	--localstatedir=/var \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \
 	--with-json-parsing
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

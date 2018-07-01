978d0e15b8270e368d8b80cbb9d3269f1082e8a0
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 20:58:49 +0300

    stage-two/gzip: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/gzip/config.mk
+++ packages/stage-two/gzip/config.mk
@@ -7,7 +7,14 @@ PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
-	--bindir=/bin \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
-CC := "${CC} "
+
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

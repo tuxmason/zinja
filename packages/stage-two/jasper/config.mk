dab2e0904b8775a7f0ac91be7f9c19d9042a6e5b
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 13:02:41 +0300

    stage-two/jasper: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/jasper/config.mk
+++ packages/stage-two/jasper/config.mk
@@ -35,5 +35,11 @@ ifeq ($(ARCH),x86_64)
 		-D CMAKE_SYSTEM_PROCESSOR=x86_64
 endif
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

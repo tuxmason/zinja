a04291545bd6381a4056399cf0212dbf28349aea
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 13:04:21 +0300

    stage-two/isl: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/isl/config.mk
+++ packages/stage-two/isl/config.mk
@@ -9,4 +9,12 @@ SRCURL := http://isl.gforge.inria.fr/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
-CC := "${CC} "
+
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

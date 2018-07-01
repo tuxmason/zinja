c45aebff8fb792ec44c0b801a82c706ab6c3c7de
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 21:01:52 +0300

    stage-two/grep: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/grep/config.mk
+++ packages/stage-two/grep/config.mk
@@ -8,8 +8,15 @@ PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--bindir=/bin \
-	--disable-nls \
         --build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \
 	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache
-CC := "${CC} "
+
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

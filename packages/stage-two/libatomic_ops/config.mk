e26369552ef0d78ce6a9c53aa7a09f4f1c93ef67
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 12:48:12 +0300

    stage-two/libatomic_ops: add pkg mgt support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libatomic_ops/config.mk
+++ packages/stage-two/libatomic_ops/config.mk
@@ -7,11 +7,20 @@ PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := https://github.com/ivmai/libatomic_ops/releases/download/v7.6.2/$(PKGSRC)
 COPTS := --prefix=/usr \
+	--enable-shared \
+	--enable-static \
 	--sysconfdir=/etc \
 	--localstatedir=/var \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \
+	--with-sysroot=$(SYSROOTDIR) \
 	--docdir=/usr/share/doc/$(PKGNAME)-$(PKGVER)
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/libatomic-ops
+ORIGSRC := libatomic-ops_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

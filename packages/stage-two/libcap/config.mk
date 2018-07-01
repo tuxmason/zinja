8211bbf2e5beafd5f6af58c036a52d2c1353dcc2
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 12:46:41 +0300

    stage-two/libcap: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libcap/config.mk
+++ packages/stage-two/libcap/config.mk
@@ -7,4 +7,12 @@ PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/$(PKGSRC)
 LIB := lib
+
 CC := "${CC} "
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

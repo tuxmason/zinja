b1209fe8058d66de6898f9e668ae50c1f3fe1f00
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 12:50:12 +0300

    stage-two/libaio: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libaio/config.mk
+++ packages/stage-two/libaio/config.mk
@@ -7,9 +7,16 @@ PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := http://ftp.debian.org/debian/pool/main/liba/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
+	--enable-static \
 	--sysconfdir=/etc \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

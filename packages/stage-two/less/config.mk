b834a2c3ddbbe0e86623660ba85fdbc720b941e2
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 12:50:53 +0300

    stage-two/less: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/less/config.mk
+++ packages/stage-two/less/config.mk
@@ -11,4 +11,12 @@ COPTS := --prefix=/usr \
 	--sysconfdir=/etc \
         --build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
-CC := "${CC} "
+
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

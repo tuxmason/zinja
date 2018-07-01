d683a7b73ef726f632f9316fe94f923dfa0abbfc
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 22:07:24 +0300

    stage-two/file: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/file/config.mk
+++ packages/stage-two/file/config.mk
@@ -9,5 +9,12 @@ SRCURL := ftp://ftp.astron.com/pub/file/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
-CC := "${CC} "
-CXX := "${CXX} "
+
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

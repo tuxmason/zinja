e0dc19e5f6daa522a62f6fd2639dac890424e144
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 12:51:37 +0300

    stage-two/kmod: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/kmod/config.mk
+++ packages/stage-two/kmod/config.mk
@@ -14,4 +14,12 @@ COPTS := --prefix=/usr \
 	--with-zlib \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
-CC := "${CC} "
+
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

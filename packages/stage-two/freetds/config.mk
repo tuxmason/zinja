4039eaa7a61e1e354579e4d6a65403e584d0b241
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 21:54:04 +0300

    stage-two/freetds: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/freetds/config.mk
@@ -0,0 +1,22 @@
+PKGNAME := freetds
+PKGVER := 1.00.80
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := ftp://ftp.freetds.org/pub/freetds/stable/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--enable-msdblib \
+	--sysconfdir=/etc \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

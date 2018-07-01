aeff3ef7c665de6ac82920f6729caa4c9bdee179
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 21:56:56 +0300

    stage-two/fontconfig: add pkg management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/fontconfig/config.mk
+++ packages/stage-two/fontconfig/config.mk
@@ -8,12 +8,20 @@ PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := https://www.freedesktop.org/software/$(PKGNAME)/release/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--disable-docs \
+	--enable-static \
 	--sysconfdir=/etc \
 	--localstatedir=/var \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \
 	--docdir=/usr/share/doc/$(PKGNAME)-$(PKGVER)
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
 CFLAGS := "-I$(SYSROOTDIR)/usr/include/freetype2"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

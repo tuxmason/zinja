3e277017fcaef4e2d67d18d332ec18752f8c9d12
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 22:06:09 +0300

    stage-two/findutils: add pkg management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/findutils/config.mk
+++ packages/stage-two/findutils/config.mk
@@ -9,5 +9,14 @@ SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \
+	--localstatedir=/var \
 	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache
-CC := "${CC} "
+
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

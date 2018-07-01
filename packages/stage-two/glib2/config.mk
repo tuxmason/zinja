48850f2d23304f5b42fdb32ab01ea4b5a35c0906
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 21:10:24 +0300

    stage-two/glib2: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/glib2/config.mk
+++ packages/stage-two/glib2/config.mk
@@ -14,7 +14,14 @@ COPTS := --prefix=/usr \
 	--with-pcre=system \
 	--cache-file=$(PKGOBJDIR)/glib2.cache
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
 CFLAGS := "-I$(SYSROOTDIR)/usr/include"
 LDFLAGS := "-L$(SYSROOTDIR)/lib -L$(SYSROOTDIR)/usr/lib"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)2
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)2
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

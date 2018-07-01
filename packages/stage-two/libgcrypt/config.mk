61789d7681a9ef2ce30ba91b5883d6904ff944c8
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 12:31:07 +0300

    stage-two/libgcrypt: add pkg management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/libgcrypt/config.mk
+++ packages/stage-two/libgcrypt/config.mk
@@ -7,6 +7,7 @@ PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := https://www.gnupg.org/ftp/gcrypt/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=/usr \
+	--enable-static \
 	--sysconfdir=/etc \
 	--localstatedir=/var \
 	--build=$(BUILDARCH) \
@@ -14,9 +15,15 @@ COPTS := --prefix=/usr \
 	--with-capabilities \
 	--with-gpg-error-prefix=$(SYSROOTDIR)/usr
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
 
 ifeq ($(ARCH),x86_64)
 	COPTS := $(COPTS) --disable-asm
 endif
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

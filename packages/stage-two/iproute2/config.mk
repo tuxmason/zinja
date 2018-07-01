78d7bca4b04e2b82da882a68bfed3c15769bce28
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 13:06:48 +0300

    stage-two/iproute2: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/iproute2/config.mk
+++ packages/stage-two/iproute2/config.mk
@@ -7,6 +7,12 @@ PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := https://www.kernel.org/pub/linux/utils/net/$(PKGNAME)/$(PKGSRC)
 COPTS := 
-CC := "${CC} "
+CC := "${CC}"
 HOSTCC := gcc
 DBMINCLUDE := $(SYSROOTDIR)/usr/include
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

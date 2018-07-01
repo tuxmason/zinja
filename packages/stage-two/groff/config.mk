e765342f0aaadfa2b8410f2efbdec113c2cde44a
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 21:00:11 +0300

    stage-two/groff: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/groff/config.mk
+++ packages/stage-two/groff/config.mk
@@ -10,6 +10,13 @@ COPTS := --prefix=/usr \
 	--with-x=no \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
-CC := "${CC} "
-CXX := "${CXX} "
+
+CC := "${CC}"
+CXX := "${CXX}"
 PAGESIZE := A4
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

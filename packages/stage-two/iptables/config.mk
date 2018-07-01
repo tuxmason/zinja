e9dfbc43ef19a02f68e6d38f65b4c5557fe1e999
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 13:04:58 +0300

    stage-two/iptables: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/iptables/config.mk
+++ packages/stage-two/iptables/config.mk
@@ -16,6 +16,13 @@ COPTS := --prefix=/usr \
 	--enable-bpf-compiler \
 	--with-kernel=$(SYSROOTDIR)/usr
 
-CC := "${CC} "
-CXX := "${CXX} "
-LDFLAGS := "-L$(SYSROOTDIR)/usr/lib -L$(SYSROOTDIR)/usr/lib"
+CC := "${CC}"
+CXX := "${CXX}"
+
+LDFLAGS := "-L$(SYSROOTDIR)/lib -L$(SYSROOTDIR)/usr/lib"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

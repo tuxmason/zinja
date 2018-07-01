1553cd051a9b6f7738c863a8031e0d3a814fbba0
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-01 22:11:03 +0300

    stage-two/e2fsprogs: add pkg management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/e2fsprogs/config.mk
+++ packages/stage-two/e2fsprogs/config.mk
@@ -17,5 +17,11 @@ COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

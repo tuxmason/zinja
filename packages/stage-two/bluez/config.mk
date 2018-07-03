867b798aec5c9e5f7fa7c27c554228ae47042942
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-04 02:21:38 +0300

    stage-two/bluez: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/bluez/config.mk
+++ packages/stage-two/bluez/config.mk
@@ -10,11 +10,19 @@ COPTS := --prefix=/usr \
 	--sysconfdir=/etc \
 	--localstatedir=/var \
 	--enable-library \
+	--enable-static \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
 CPPFLAGS := "-I$(SYSROOTDIR)/usr/include/glib-2.0 \
 	-I$(SYSROOTDIR)/usr/lib/glib-2.0/include"
 LDFLAGS := "-L$(SYSROOTDIR)/lib -L$(SYSROOTDIR)/usr/lib"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

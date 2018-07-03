a6ad8ef6ec7333ccb01e5ce1cd67a82f32b67f47
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-04 02:43:52 +0300

    stage-two/attr: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/attr/config.mk
+++ packages/stage-two/attr/config.mk
@@ -18,4 +18,12 @@ COPTS := --prefix=/ \
 	--datadir=/usr/share \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
-CC := "${CC} "
+
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

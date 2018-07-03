155b059d78a5303b2344c8def1cc26a8bb6a67fc
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-04 02:48:51 +0300

    stage-two/acl: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/acl/config.mk
+++ packages/stage-two/acl/config.mk
@@ -13,4 +13,12 @@ COPTS := --prefix=/usr \
 	--mandir=/usr/share/man \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
-CC := "${CC} "
+
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

e247b62d54e7c0586b093ef8f1e20083a6f169da
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-04 02:13:10 +0300

    stage-two/cracklib-dict: add pkg mgt support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/cracklib-dict/config.mk
+++ packages/stage-two/cracklib-dict/config.mk
@@ -5,11 +5,10 @@ PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
-COPTS := --prefix=/usr \
-	--disable-static \
-	--without-python \
-	--build=$(BUILDARCH) \
-	--host=$(TARGETARCH) \
-	--with-default-dict=/lib/cracklib/pw_dict
-CC := "${CC} "
-CXX := "${CXX} "
+COPTS :=
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

e4d3aed04074f5f075bdf3ddbd2bbd126947e67f
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-07-04 02:19:12 +0300

    stage-two/check: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/check/config.mk
+++ packages/stage-two/check/config.mk
@@ -9,3 +9,12 @@ SRCURL := https://github.com/libcheck/check/releases/download/$(PKGVER)/$(PKGSRC
 COPTS := --prefix=/usr \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
+
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

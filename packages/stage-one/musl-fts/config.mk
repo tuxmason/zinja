acf346f110d52afadabe6920c131cfa447c4a244
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-09-09 16:06:35 +0300

    stage-one/musl-fts: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-one/musl-fts/config.mk
+++ packages/stage-one/musl-fts/config.mk
@@ -12,3 +12,9 @@ COPTS := --prefix=/usr \
 
 CC := "${CC} "
 CXX := "${CXX} "
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

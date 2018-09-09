289766f2ec29941328f1c8283ecb3077a9d329f6
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-09-09 15:37:47 +0300

    stage-one/pl-develsymdump: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-one/pl-develsymdump/config.mk
@@ -0,0 +1,8 @@
+PKGNAME := Devel-Symdump
+PKGVER := 2.18
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://search.cpan.org/CPAN/authors/id/A/AN/ANDK/$(PKGSRC)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-obj

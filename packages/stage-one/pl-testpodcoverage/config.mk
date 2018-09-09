b16cf04f0a23df8f51d4a8e167e3fb218f117432
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-09-09 15:42:45 +0300

    stage-one/pl-testpodcoverage: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-one/pl-testpodcoverage/config.mk
@@ -0,0 +1,8 @@
+PKGNAME := Test-Pod-Coverage
+PKGVER := 1.10
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://search.cpan.org/CPAN/authors/id/N/NE/NEILB/Test-Pod-Coverage-1.10.tar.gz
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-obj

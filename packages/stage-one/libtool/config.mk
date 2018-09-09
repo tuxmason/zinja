a18c1a72b82639b6a61517c55eaf357f66701e3d
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-09-09 15:56:07 +0300

    stage-one/libtool: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-one/libtool/config.mk
@@ -0,0 +1,12 @@
+PKGNAME := libtool
+PKGVER := 2.4.6
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-stage-one-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
+COPTS := --prefix=$(CROSSTOOLS) \
+        --disable-static \
+	--host=$(TARGETARCH) \
+	--program-prefix=$(TARGETARCH)-

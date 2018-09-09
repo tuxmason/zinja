79a032cc50790177807fd68cd9e1da80c295b732
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-09-09 15:33:57 +0300

    stage-one/pl-configfile: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-one/pl-configfile/config.mk
@@ -0,0 +1,8 @@
+PKGNAME := Config-File
+PKGVER := 1.50
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://search.cpan.org/CPAN/authors/id/G/GW/GWOLF/$(PKGSRC)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-obj

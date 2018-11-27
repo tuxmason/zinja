47465284ceea93fd3c9e6089bcda0d6206ab22ef
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-11-27 11:25:49 +0300

    stage-two/kernel: add aarch64, pkg managment
    - add support for aarch64 and package management
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/kernel/config.mk
+++ packages/stage-two/kernel/config.mk
@@ -2,15 +2,33 @@ PKGNAME := linux
 PKGVER := 4.9.66
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
-PATCHLIST := $(PATCHDB)/kernel/list.txt
-PATCHDIR := $(PATCHDB)/kernel
-ifeq ($(ARCH),arm)
-	CONF := $(PATCHDB)/kernel/kconfig.arm
-	KIMAGE := zImage
-endif
-ifeq ($(ARCH),x86_64)
-	CONF := $(PATCHDB)/kernel/kconfig.x86_64
-	KIMAGE := bzImage
-endif
-PKGOBJDIR := $(TCBUILDROOT)/kernel-obj
+PATCHDIR := $(PATCHDB)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-kernel-obj
 SRCURL := https://cdn.kernel.org/pub/linux/kernel/v4.x/$(PKGSRC)
+
+ifeq ($(ARCH),arm)
+	KARCH := $(ARCH)
+	KIMAGE := zImage
+	CONF := $(PATCHDIR)/kconfig.$(ARCH)
+	PATCHLIST := $(PATCHDIR)/list.$(ARCH).txt
+endif
+
+ifeq ($(ARCH),aarch64)
+	KARCH := arm64
+	KIMAGE := Image
+	CONF := $(PATCHDIR)/kconfig.$(ARCH)
+	PATCHLIST := $(PATCHDIR)/list.$(ARCH).txt
+endif
+
+ifeq ($(ARCH),x86_64)
+	KARCH := $(ARCH)
+	KIMAGE := bzImage
+	CONF := $(PATCHDIR)/kconfig.$(ARCH)
+	PATCHLIST := $(PATCHDIR)/list.$(ARCH).txt
+endif
+
+PKGDIR := $(PKGDB)/$(PKGNAME)-$(PKGVER)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)-$(PKGVER)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

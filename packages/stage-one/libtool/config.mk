b881d03bb4604a6ee66ac6406642a2cfca1bcbe5
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2018-11-29 18:50:27 +0300

    stage-one/libtool: build cross libtools
    some distros e.g Ubuntu have a 'funky' libtool which breaks cross linking
    for some packages like util-linux;
    
    > libtool: link: aarch64-linux-musl-gcc -fsigned-char -fno-common -Wall -Werror=sequence-point -Wextra -Wmissing-declarations -Wmissing-parameter-type -Wmissing-prototypes -Wno-missing-field-initializers -Wredundant-decls -Wsign-compare -Wtype-limits -Wuninitialized -Wunused-but-set-parameter -Wunused-but-set-variable -Wunused-parameter -Wunused-result -Wunused-variable -Wnested-externs -Wpointer-arith -Wstrict-prototypes -Wimplicit-function-declaration -I./libmount/src -g -O2 -o .libs/mount sys-utils/mount-mount.o  -L/home/zinja/zinja-valpha-20181127-ARMHF_AARCH64/sysroot/lib -L/home/zinja/zinja-valpha-20181127-ARMHF_AARCH64/sysroot/usr/lib ./.libs/libcommon.a ./.libs/libmount.so
    > /home/zinja/zinja-valpha-20181127-ARMHF_AARCH64/cross-tools.aarch64/lib/gcc/aarch64-linux-musl/7.2.0/../../../../aarch64-linux-musl/bin/ld: warning: libblkid.so.1, needed by ./.libs/libmount.so, not found (try using -rpath or -rpath-link)
    > ./.libs/libmount.so: undefined reference to `blkid_probe_enable_superblocks@BLKID_2.17'
    > ./.libs/libmount.so: undefined reference to `blkid_probe_set_superblocks_flags@BLKID_2.17'
    > ...
    > collect2: error: ld returned 1 exit status
    > Makefile:6429: recipe for target 'mount' failed
    
    ensure that we build and use our own version of libtool
    
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-one/libtool/config.mk
+++ packages/stage-one/libtool/config.mk
@@ -3,10 +3,8 @@ PKGVER := 2.4.6
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.xz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-stage-one-obj
-PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
-PATCHDIR := $(PATCHDB)/$(PKGNAME)
+PATCHDIR := $(PATCHDB)/$(PKGNAME)-cross
+PATCHLIST := $(PATCHDIR)/list.txt
 SRCURL := http://ftp.gnu.org/gnu/$(PKGNAME)/$(PKGSRC)
 COPTS := --prefix=$(CROSSTOOLS) \
-        --disable-static \
-	--host=$(TARGETARCH) \
-	--program-prefix=$(TARGETARCH)-
+        --disable-static

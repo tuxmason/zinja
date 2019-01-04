181d8fc550803b90a31658f2d112cd613bd4d8c6
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2019-01-04 23:09:23 +0300

    stage-two/dnprogs: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/dnprogs/config.mk
@@ -0,0 +1,19 @@
+PKGNAME := dnprogs
+PKGVER := 2.65
+PKGSRC := $(PKGNAME)_$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://deb.debian.org/debian/pool/main/d/dnprogs/$(PKGSRC)
+COPTS := 
+
+CC := "${CC}"
+CXX := "${CXX}"
+AR := "${AR}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

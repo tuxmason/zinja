07ae2a3a2a706cfbeee3f762a2695560a991d298
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2019-01-05 01:18:53 +0300

    stage-two/libzeitgeist: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/libzeitgeist/config.mk
@@ -0,0 +1,20 @@
+PKGNAME := libzeitgeist
+PKGVER := 0.3.18
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://launchpad.net/libzeitgeist/0.3/0.3.18/+download/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

078002faa07c6995ce7cad0c275bdcda9c8e694b
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2019-01-04 19:08:59 +0300

    xorg/gdk-pixbuf: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/xorg/gdk-pixbuf/config.mk
+++ packages/stage-two/xorg/gdk-pixbuf/config.mk
@@ -8,16 +8,22 @@ PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := http://ftp.gnome.org/pub/gnome/sources/$(PKGNAME)/2.36/$(PKGSRC)
 COPTS := --prefix=/usr \
 	--with-x11 \
+	--enable-static \
 	--with-libjasper \
 	--enable-debug=no \
 	--disable-glibtest \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH) \
 	--enable-introspection=no \
 	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache
 
-CC := "${CC} "
-CXX := "${CXX} "
-PKG_CONFIG_SYSROOT_DIR := $(SYSROOTDIR)
-LDFLAGS := "-L$(SYSROOTDIR)/lib -L$(SYSROOTDIR)/usr/lib"
-PKG_CONFIG_PATH := "$(SYSROOTDIR)/usr/lib/pkgconfig:$(SYSROOTDIR)/usr/share/pkgconfig"
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/xorg/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

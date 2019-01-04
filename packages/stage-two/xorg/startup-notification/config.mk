7b58193522287f10e71dd1ea65b482f6e941318d
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2019-01-04 20:44:35 +0300

    xorg/startup-notification: add pkg mgt support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/xorg/startup-notification/config.mk
+++ packages/stage-two/xorg/startup-notification/config.mk
@@ -13,5 +13,11 @@ COPTS := --prefix=/usr \
 	--host=$(TARGETARCH) \
 	--cache-file=$(PKGOBJDIR)/$(PKGNAME).cache
 
-CC := "${CC} "
-CXX := "${CXX} "
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/xorg/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

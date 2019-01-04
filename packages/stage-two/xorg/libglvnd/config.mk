106523f3e77260173c25ffcbf3cb4e4fd113ca6d
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2019-01-04 19:19:56 +0300

    xorg/libglvnd: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/xorg/libglvnd/config.mk
+++ packages/stage-two/xorg/libglvnd/config.mk
@@ -1,17 +1,22 @@
 PKGNAME := libglvnd
-PKGVER := 1.0.0
+PKGVER := 1.1.0
 PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
 PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
-SRCURL := https://github.com/NVIDIA/libglvnd/archive/v1.0.0.tar.gz
+SRCURL := https://github.com/NVIDIA/libglvnd/archive/v1.1.0.tar.gz
 COPTS := --prefix=/usr \
 	--sysconfdir=/etc \
 	--localstatedir=/var \
 	--build=$(BUILDARCH) \
 	--host=$(TARGETARCH)
 
-CC := "${CC} "
-CXX := "${CXX} "
-LDFLAGS := "-L$(SYSROOTDIR)/lib -L$(SYSROOTDIR)/usr/lib"
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/xorg/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

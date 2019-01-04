4ecb0acd38e91d93969094ae0d09ecb5bbaec7b4
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2019-01-04 23:55:56 +0300

    stage-two/icon-naming-util: intial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/icon-naming-utils/config.mk
@@ -0,0 +1,23 @@
+PKGNAME := icon-naming-utils
+PKGVER := 0.8.90
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.bz2
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := http://tango.freedesktop.org/releases/$(PKGSRC)
+COPTS := --prefix=/usr \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--libexecdir=/usr/bin \
+	--host=$(TARGETARCH)
+
+CC := "${CC}"
+CXX := "${CXX}"
+
+PKGDIR := $(PKGDB)/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

5e049ef8558e7b57de48f057682e134bbe1cca5a
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2019-01-05 01:33:20 +0300

    xorg/libglvnd-static: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/xorg/libglvnd-static/config.mk
@@ -0,0 +1,25 @@
+PKGNAME := libglvnd
+PKGVER := 1.1.0
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-static-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://github.com/NVIDIA/libglvnd/archive/v1.1.0.tar.gz
+COPTS := --prefix=/usr \
+	--enable-static \
+	--sysconfdir=/etc \
+	--localstatedir=/var \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
+
+CC := "${CC} -static"
+CXX := "${CXX} -static"
+
+LDFLAGS := "--static"
+
+PKGDIR := $(PKGDB)/xorg/$(PKGNAME)-static
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)-static
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

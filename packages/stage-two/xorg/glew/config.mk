f888f1d1ea5d66a9bcf59b1e56359bf5d9204313
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2019-01-04 19:10:28 +0300

    xorg/glew: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/xorg/glew/config.mk
+++ packages/stage-two/xorg/glew/config.mk
@@ -6,10 +6,16 @@ PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
 SRCURL := https://sourceforge.net/projects/glew/files/glew/$(PKGVER)/$(PKGSRC)
-COPTS := 
+COPTS :=
 
 CC := "${CC}"
 CXX := "${CXX}"
-LD := $(CC) 
+LD := $(CC)
 STRIP := "${STRIP}"
 CFLAGS := "$$CFLAGS -fPIC"
+
+PKGDIR := $(PKGDB)/xorg/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg

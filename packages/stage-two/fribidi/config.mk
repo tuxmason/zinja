8cbf45ec3c9b939c77c4ba87129ad89a90f17b6a
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2019-01-04 15:54:58 +0300

    stage-two/fribidi: add package management support
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

--- packages/stage-two/fribidi/config.mk
+++ packages/stage-two/fribidi/config.mk
@@ -5,17 +5,14 @@ PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
 PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
 PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
 PATCHDIR := $(PATCHDB)/$(PKGNAME)
-MESON_CROSS := $(PKGOBJDIR)/meson_cross.txt
-SRCURL := https://github.com/$(PKGNAME)/$(PKGNAME)/archive/$(PKGVER)/$(PKGSRC)
+SRCURL := ftp://sourceware.org/pub/libffi/$(PKGSRC)
 COPTS := --prefix=/usr \
-	--sysconfdir=/etc \
-	--buildtype=release \
-	-D 'docs=true'
+	--enable-static \
+	--build=$(BUILDARCH) \
+	--host=$(TARGETARCH)
 
-BUILD_AR := "ar"
-BUILD_CC := "gcc"
-BUILD_CXX := "g++"
-BUILD_RANLIB := "ranlib"
+CC := "${CC}"
+CXX := "${CXX}"
 
 PKGDIR := $(PKGDB)/$(PKGNAME)
 ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz

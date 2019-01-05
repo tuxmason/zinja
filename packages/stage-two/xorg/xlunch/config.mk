ba7239186565043d1edbcba2597e1d7d32a34418
Mwanguhya Daniel Murungi <tuxmason@gmail.com>
2019-01-05 04:41:08 +0300

    xorg/xlunch: initial import
    Signed-off-by: Mwanguhya Daniel Murungi <tuxmason@gmail.com>

---
+++ packages/stage-two/xorg/xlunch/config.mk
@@ -0,0 +1,20 @@
+PKGNAME := xlunch
+PKGVER := 4.1.1
+PKGSRC := $(PKGNAME)-$(PKGVER).tar.gz
+PKGSRCDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)
+PKGOBJDIR := $(TCBUILDROOT)/$(PKGNAME)-$(PKGVER)-obj
+PATCHLIST := $(PATCHDB)/$(PKGNAME)/list.txt
+PATCHDIR := $(PATCHDB)/$(PKGNAME)
+SRCURL := https://github.com/Tomas-M/xlunch/archive/v4.1.1.tar.gz
+COPTS :=
+
+CC := "${CC}"
+CXX := "${CXX}"
+
+CPPFLAGS := "-I$(SYSROOTDIR)/usr/include"
+
+PKGDIR := $(PKGDB)/xorg/$(PKGNAME)
+ORIGSRC := $(PKGNAME)_$(PKGVER).orig.tar.xz
+PKGROOT := $(DISTRIBROOT)/$(PKGNAME)
+DISTRIBSRC := $(PKGROOT)/$(PKGNAME)-$(PKGVER)
+PKGBINDIR := $(DISTRIBSRC)/debian/pkg
